a0e6c91c5da02701e5235d122e06c0267573ff68
Earley <earleyj@m-d25q90jsfy13.local>
2017-03-21 11:51:08 -0400

    record format quickfix to accomodate array values

--- src/modules/records/components/RecordField/index.js
+++ src/modules/records/components/RecordField/index.js
@@ -3,19 +3,24 @@ import React from 'react';
 import { Icon } from '../../../core';
 
 function RecordField({ field }) {
-  let value = field.value
+  let fieldValue = field.value
 
-  if (Array.isArray(field.value)) {
-    value = field.value.join(', ')
+  if (field.uid === 'format') {
+    if (Array.isArray(field.value)) {
+      fieldValue = field.value.map((fieldString, index) => (
+        <span className="record-field-value-item" key={index}><Icon name={fieldString.toLowerCase()} />{fieldString}</span>
+      ))
+    } else {
+      fieldValue = <span className="record-field-value-item"><Icon name={field.value.toLowerCase()} />{field.value}</span>
+    }
   }
 
   const uniqueFieldClassName = 'record-field record-field-uid-' + field.uid
-  const iconName = field.value.toLowerCase()
 
   return (
     <div className={uniqueFieldClassName}>
       <dt className="record-field-name">{field.name}</dt>
-      <dd className="record-field-value"><Icon name={iconName} />{value}</dd>
+      <dd className="record-field-value">{fieldValue}</dd>
     </div>
   );
 }
