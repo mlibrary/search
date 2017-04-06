e44d8559d14c712e653932d422534c6d6b4912d3
Earley <earleyj@m-d25q90jsfy13.local>
2017-04-06 12:51:58 -0400

    bugfix

--- src/modules/records/components/RecordField/index.js
+++ src/modules/records/components/RecordField/index.js
@@ -3,24 +3,30 @@ import React from 'react';
 import { Icon } from '../../../core';
 
 function RecordField({ field }) {
-  let fieldValue = field.value
   const uniqueFieldClassName = 'record-field record-field-uid-' + field.uid
 
   if (field.uid === 'format') {
     if (Array.isArray(field.value)) {
-      fieldValue = field.value.map((fieldString, index) => (
-        <span className="record-field-value-item" key={index}><Icon name={fieldString.toLowerCase()} />{fieldString}</span>
-      ))
+      return (
+        <div className={uniqueFieldClassName}>
+          <dt className="record-field-name">{field.name}</dt>
+          <dd className="record-field-value">
+            {field.value.map((fieldString, index) => (
+              <span className="record-field-value-item" key={index}><Icon name={fieldString.toLowerCase()} />{fieldString}</span>
+            ))}
+          </dd>
+        </div>
+      )
     } else {
-      fieldValue = <span className="record-field-value-item"><Icon name={field.value.toLowerCase()} />{field.value}</span>
+      return (
+        <div className={uniqueFieldClassName}>
+          <dt className="record-field-name">{field.name}</dt>
+          <dd className="record-field-value">
+            <span className="record-field-value-item"><Icon name={field.value.toLowerCase()} />{field.value}</span>
+          </dd>
+        </div>
+      )
     }
-
-    return (
-      <div className={uniqueFieldClassName}>
-        <dt className="record-field-name">{field.name}</dt>
-        <dd className="record-field-value">{fieldValue}</dd>
-      </div>
-    )
   }
 
   if (Array.isArray(field.value)) {
