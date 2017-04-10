c4f0702ba9b6bdb62e4e583150c279f9508d221b
Earley <earleyj@m-d25q90jsfy13.local>
2017-04-10 16:07:56 -0400

    trim string component

---
+++ src/modules/core/components/TrimString/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+class TrimString extends React.Component {
+  state = {
+    show: false
+  }
+
+  handleShowToggleClick() {
+    this.setState({
+      show: !this.state.show
+    })
+  }
+
+  render() {
+    const string = this.props.string
+    const trimLength = this.props.trim
+
+    return (
+      <span>
+        {string}
+        {this.state.show ? (
+          {string}
+        ) : (
+          {string.trim(trimLength)}
+        )}
+        <ShowHideButton
+          handleOnClick={handleShowToggleClick.bind(this)}
+          show: this.state.show
+        />
+      </span>
+    )
+  }
+}
+
+const ShowHideButton = ({ handleOnClick, show}) => {
+  return (
+    <button
+      onClick={() => handleOnClick()}
+      className="button-link-light">
+      {show ? 'Less' : 'More'}
+    </button>
+  )
+}
+
+export default ShowAllList
