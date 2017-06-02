1556a5b631992d62ee8eb4f21cb483de4d8a4a33
Earley <earleyj@m-d25q90jsfy13.local>
2017-06-02 10:17:22 -0400

    connected switch bugfix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
+import {
+  connect,
+  Provider
+} from 'react-redux'
+
 import {
   Route,
   Switch,
@@ -21,11 +25,26 @@ import {
 import store from './store'
 import history from './history'
 
+/*
+ * Connected Switch: Quirk/Bugfix
+ *
+ * Why does it help to connect Switch?
+ * Because it passes down the location prop which
+ * is then used by Route instead of context.
+ *
+ * Details:
+ * https://github.com/ReactTraining/react-router/issues/5072#issuecomment-300636461
+ */
+const mapStateToProps = state => {
+	return { location: state.router.location };
+};
+const ConnectedSwitch = connect(mapStateToProps)(Switch);
+
 const App = () => (
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <URLSearchQueryWrapper>
-        <Switch>
+        <ConnectedSwitch>
           <Route path="/" exact render={() => (
             <Redirect to={`/everything`} />
           )}/>
@@ -40,7 +59,7 @@ const App = () => (
             )
           }}/>
           <Route component={NoMatch} />
-        </Switch>
+        </ConnectedSwitch>
       </URLSearchQueryWrapper>
     </ConnectedRouter>
   </Provider>
