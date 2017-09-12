14cf899cbbfb2639df6a2b130afc3fca815f8dee
Jon Earley <jonathan.a.earley@gmail.com>
2017-09-12 14:11:30 -0400

    added copyright html symbol #41

--- src/modules/core/components/Footer/index.js
+++ src/modules/core/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="site-footer">
-      <p>Copyright 2017 <a href="http://www.regents.umich.edu/">Regents of the University of Michigan</a>. For details and exceptions, see the <a href="https://www.lib.umich.edu/library-administration/library-copyright-statement">Library Copyright Statement</a>.</p>
+      <p>©2017 <a href="http://www.regents.umich.edu/">Regents of the University of Michigan</a>. For details and exceptions, see the <a href="https://www.lib.umich.edu/library-administration/library-copyright-statement">Library Copyright Statement</a>.</p>
     </footer>
   )
 }
