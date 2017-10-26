6919a2f44b86cb938a58da3c3e94d81759211812
Earley <earleyj@m-d25q90jsfy13.local>
2017-10-26 13:58:14 -0400

    Removed unnecessary imports

--- src/modules/pages/components/RoadMapPage/index.js
+++ src/modules/pages/components/RoadMapPage/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const marginBottomNone = { margin: 0 }
 const marginBottom = {
--- src/modules/search/components/HelpContent/index.js
+++ src/modules/search/components/HelpContent/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const HelpContent = () => (
   <div className="container container-narrow">
