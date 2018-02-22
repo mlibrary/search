1ff4f028de0ffe67447ff02241e764b6344dc092
Jon Earley <jonathan.a.earley@gmail.com>
2018-02-22 10:50:04 -0500

    Fixed flex display element containers

--- src/modules/specialists/components/Specialist/index.js
+++ src/modules/specialists/components/Specialist/index.js
@@ -6,14 +6,16 @@ const Specialist = ({ person }) => (
     <div>
       <img src={person.picture} alt="" className="specialist__picture" />
     </div>
-    <header>
-      <h1 className="specialist__heading"><a href={person.url} className="specialist__url">{person.name}</a><Icon name="launch" /></h1>
-    </header>
-    <section>
-      <p className="specialist__job-title">{person.job_title}</p>
-      <p className="specialist__phone">{person.phone}</p>
-      <a className="specialist__email" href={`mailto:${person.email}`}>{person.email}</a>
-    </section>
+    <div>
+      <header>
+        <h1 className="specialist__heading"><a href={person.url} className="specialist__url">{person.name}</a><Icon name="launch" /></h1>
+      </header>
+      <section>
+        <p className="specialist__job-title">{person.job_title}</p>
+        <p className="specialist__phone">{person.phone}</p>
+        <a className="specialist__email" href={`mailto:${person.email}`}>{person.email}</a>
+      </section>
+    </div>
   </article>
 )
 
