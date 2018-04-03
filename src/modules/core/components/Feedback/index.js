7fd997fdfeadd004233f36e375bf60d82e7c1800
Jon Earley <jonathan.a.earley@gmail.com>
2018-04-03 12:57:35 -0400

    Added feedback feature

--- src/modules/core/components/Feedback/index.js
+++ src/modules/core/components/Feedback/index.js
@@ -1,14 +1,68 @@
 import React from 'react';
 
 class Feedback extends React.Component {
+  state = {
+    form: false
+  }
+
+  handleFormClick = () => {
+    // Makes the form visible or hidden
+    this.setState({ form: !this.state.form })
+  }
+
+  renderFeedbackForm = () => {
+    if (this.state.form) {
+      return (
+        <form className="feedback-form">
+          <fieldset className="feedback-form-fieldset">
+            <legend>
+              <h1 class="feedback-heading">
+                Help us improve Library Search
+              </h1>
+
+              <p>Let us know what is wrong with this page.</p>
+
+              <button className="button-link feedback-close-button" onClick={this.handleFormClick}>Close</button>
+            </legend>
+
+            <div className="form-group">
+              <label for="feedback-doing">What were you doing?</label>
+              <input type="text" id="feedback-doing" />
+            </div>
+
+            <div className="form-group">
+              <label for="feedback-wrong">What went wrong?</label>
+              <input type="text" id="feedback-wrong" />
+            </div>
+
+            <input type="submit" value="Send" className="button" />
+          </fieldset>
+        </form>
+      )
+    }
+
+    return null
+  }
+
+  renderFeedbackOptions = () => {
+    if (!this.state.form) {
+      return (
+        <div className="feedback-options">
+          <p><b>Was this page helpful?</b> <a href="" className="underline feedback-link">Yes</a> <a href="" className="underline">No</a></p>
+
+          <button className="button-link underline" onClick={this.handleFormClick}>Is there anything wrong with this page?</button>
+        </div>
+      )
+    }
+  }
+
   render() {
 
     return (
-      <section className="container container-medium feedback-container">
-        <p className="text-small">Was this page helpful? <a href="" className="underline feedback-link">Yes</a> <a href="" className="underline">No</a></p>
-
-        <a href="" className="underline text-small">Is there anything wrong with this page?</a>
-      </section>
+      <div className="container container-narrow feedback-container">
+        {this.renderFeedbackOptions()}
+        {this.renderFeedbackForm()}
+      </div>
     )
   }
 }
--- src/modules/core/components/Footer/index.js
+++ src/modules/core/components/Footer/index.js
@@ -7,9 +7,9 @@ import {
 const Footer = () => {
   return (
     <footer className="site-footer">
-      <div className="container container-medium">
+      <div className="container container-narrow">
         <AskALibrarian />
-        
+
         <p>©2017 Regents of the University of Michigan. For details and exceptions, see the <a href="https://www.lib.umich.edu/library-administration/library-copyright-statement">Copyright Policy</a>.</p>
       </div>
     </footer>
