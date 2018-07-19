d1333a88c885604e1f3b014b4d520b22d68891ee
Jon Earley <jonathan.a.earley@gmail.com>
2018-07-19 17:28:16 -0400

    Added Modal component SEARCH-605

--- src/modules/reusable/index.js
+++ src/modules/reusable/index.js
@@ -2,11 +2,13 @@ import Header from './components/Header'
 import Alert from './components/Alert'
 import Button from './components/Button'
 import Tag from './components/Tag'
+import Modal from './components/Modal'
 
 
 export {
   Header,
   Alert,
   Button,
-  Tag
+  Tag,
+  Modal
 }
