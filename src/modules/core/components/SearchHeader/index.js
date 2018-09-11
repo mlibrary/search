dfca7dd74243d162358a84b0cee7b2b6681a0c01
Jon Earley <jonathan.a.earley@gmail.com>
2018-09-11 11:45:06 -0400

    Removed launch message

--- src/modules/core/components/SearchHeader/index.js
+++ src/modules/core/components/SearchHeader/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { SiteMessage } from '../../../core'
 import { Header } from '../../../reusable'
 import config from '../../../../config'
 
@@ -14,12 +13,6 @@ class SearchHeader extends React.Component {
 
     return  (
       <div role="banner">
-        <SiteMessage>
-          <div className="container container-medium">
-            <p>U-M Library Search launched in July 2018. <a href="https://umich.qualtrics.com/jfe/form/SV_bCwYIKueEXs8wBf">Please let us know if you have feedback</a>.</p>
-          </div>
-        </SiteMessage>
-
         <Header
           name="Search"
           url="/"
