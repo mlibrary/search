811b66025764a07b16740c9a21cb660f9cd40d6f
Jon Earley <jonathan.a.earley@gmail.com>
2018-09-18 10:04:08 -0400

    add in site message

--- src/modules/core/components/SearchHeader/index.js
+++ src/modules/core/components/SearchHeader/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Header } from '../../../reusable'
 import config from '../../../../config'
+import { SiteMessage } from '../../../core'
 
 class SearchHeader extends React.Component {
   render() {
@@ -13,6 +14,11 @@ class SearchHeader extends React.Component {
 
     return  (
       <div role="banner">
+        <SiteMessage>
+          <div className="container container-medium">
+            <p>U-M Library Search launched in July 2018. <a href="https://umich.qualtrics.com/jfe/form/SV_bCwYIKueEXs8wBf">Please let us know if you have feedback</a>.</p>
+          </div>
+        </SiteMessage>
         <Header
           name="Search"
           url="/"
