2466287ed589570d67f7070dd9a032242c88d8d8
Jon Earley <jonathan.a.earley@gmail.com>
2018-09-25 12:13:18 -0400

    removed site message

--- src/modules/core/components/SearchHeader/index.js
+++ src/modules/core/components/SearchHeader/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Header } from '../../../reusable'
 import config from '../../../../config'
-import { SiteMessage } from '../../../core'
 
 class SearchHeader extends React.Component {
   render() {
@@ -14,11 +13,6 @@ class SearchHeader extends React.Component {
 
     return  (
       <div role="banner">
-        <SiteMessage>
-          <div className="container container-medium">
-            <p>U-M Library Search launched in July 2018. <a href="https://umich.qualtrics.com/jfe/form/SV_bCwYIKueEXs8wBf">Please let us know if you have feedback</a>.</p>
-          </div>
-        </SiteMessage>
         <Header
           name="Search"
           url="/"
