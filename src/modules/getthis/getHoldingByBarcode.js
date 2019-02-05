c9217ec3bd6da2f80de1f9ee344446bb8a5d4b87
Jon Earley <jonathan.a.earley@gmail.com>
2019-02-05 13:22:47 -0500

    Fixed issue showing the incorrect get this holding -- SEARCH-825.

--- src/modules/getthis/getHoldingByBarcode.js
+++ src/modules/getthis/getHoldingByBarcode.js
@@ -1,3 +1,16 @@
+/*
+  What does this do?
+  
+  Walk through the ResourceAccess aka holdings data
+  structure and pick out the holding that matches
+  the passed in barcode. Remove everything else, but keep
+  the holding container information. In other words,
+  it should maintain the resource acccess data shape, but
+  only contain the holding that matches the barcode.
+
+  If it can't be found, return undefined.
+*/
+
 export default function(holdings, barcode) {
   let result
 
@@ -6,7 +19,10 @@ export default function(holdings, barcode) {
       holding.rows.forEach(row => {
         row.forEach(cell => {
           if (cell.to && cell.to.action === 'get-this' && cell.to.barcode === barcode) {
-            result = [].concat(holding)
+            result = [].concat({
+              ...holding,
+              rows: [row]
+            })
           }
         })
       })
