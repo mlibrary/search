93723122a21e47bc3793313313ecc0c08e3e8d5b
Jon Earley <jonathan.a.earley@gmail.com>
2019-04-02 12:54:42 -0400

    Add Get This GA events -- SEARCH-876.

--- src/modules/getthis/components/GetThisOption/index.js
+++ src/modules/getthis/components/GetThisOption/index.js
@@ -1,61 +1,69 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import GetThisFindIt from '../GetThisFindIt'
 import {
   GetThisForm
 } from '../../../getthis'
+import ReactGA from 'react-ga'
 
 const createMarkup = (html) => {
   return { __html: html };
 };
 
-class GetThisOption extends React.Component {
-  render() {
-    const { option } = this.props
-
-    return (
-      <details className="get-this-option">
-        <summary className="get-this-option-summary">
-          <h3 className="get-this-option-heading"><span className="get-this-option-heading-text">{option.label}</span><span className="get-this-option-subheading">{option.service_type && (<span> {option.service_type} </span>)}({option.duration})</span></h3>
-        </summary>
-
-        <div className="get-this-option-details-container">
-          {option.form ? (
-            <div className="get-this-option-left-half">
-              {option.orientation && (
-                <div dangerouslySetInnerHTML={createMarkup(option.orientation)} />
-              )}
-              <GetThisForm form={option.form} />
-            </div>
-          ) : (
-            <React.Fragment>
-              {option.service_type === 'Self Service' && (
-                <div className="get-this-option-left-half">
-                  <GetThisFindIt />
-                </div>
-              )}
-            </React.Fragment>
-          )}
-
-          {option.description && (
-            <div className="get-this-option-right-half">
-              <div className="get-this-policies-container">
-                {option.description.heading && (
-                  <h4 className="get-this-policies-heading">{option.description.heading}</h4>
-                )}
-                {option.description.content && (
-                  <ul className="get-this-policies-list">
-                    {option.description.content.map((policy, key) => (
-                      <li key={key} dangerouslySetInnerHTML={createMarkup(policy)} />
-                    ))}
-                  </ul>
-                )}
-              </div>
-            </div>
-          )}
-        </div>
-      </details>
-    )
+function GetThisOption({ option }) {
+  const detailsRef = useRef(null);
+  const onSummaryClick = () => {
+    // If <details> was not open, then the user selected to open it.
+    const userIntent = detailsRef.current.hasAttribute('open') ? 'Close' : 'Open'
+    ReactGA.event({
+      action: 'Get This Options',
+      category: 'Item Request',
+      label: `${option.label} -- ${userIntent}`
+    })
   }
+
+  return (
+    <details className="get-this-option" ref={detailsRef}>
+      <summary className="get-this-option-summary" onClick={onSummaryClick}>
+        <h3 className="get-this-option-heading"><span className="get-this-option-heading-text">{option.label}</span><span className="get-this-option-subheading">{option.service_type && (<span> {option.service_type} </span>)}({option.duration})</span></h3>
+      </summary>
+
+      <div className="get-this-option-details-container">
+        {option.form ? (
+          <div className="get-this-option-left-half">
+            {option.orientation && (
+              <div dangerouslySetInnerHTML={createMarkup(option.orientation)} />
+            )}
+            <GetThisForm label={option.label} form={option.form} />
+          </div>
+        ) : (
+          <React.Fragment>
+            {option.service_type === 'Self Service' && (
+              <div className="get-this-option-left-half">
+                <GetThisFindIt />
+              </div>
+            )}
+          </React.Fragment>
+        )}
+
+        {option.description && (
+          <div className="get-this-option-right-half">
+            <div className="get-this-policies-container">
+              {option.description.heading && (
+                <h4 className="get-this-policies-heading">{option.description.heading}</h4>
+              )}
+              {option.description.content && (
+                <ul className="get-this-policies-list">
+                  {option.description.content.map((policy, key) => (
+                    <li key={key} dangerouslySetInnerHTML={createMarkup(policy)} />
+                  ))}
+                </ul>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+    </details>
+  )
 }
 
 export default GetThisOption
