e820127bbe7b853197ad3c91d6b40c9e6efbffc4
Jon Earley <jonathan.a.earley@gmail.com>
2019-10-31 13:04:53 -0400

    .

--- src/modules/filters/components/Filters/index.js
+++ src/modules/filters/components/Filters/index.js
@@ -275,8 +275,6 @@ function FilterGroupMultiselect({ filters, group, uid, uuid, activeFilters }) {
     return null;
   }
 
-  console.log("uuid", uuid);
-
   return (
     <AccordionItem uuid={uuid} key={uuid}>
       <AccordionItemState>
