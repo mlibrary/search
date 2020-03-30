26822f5bbca96786013733e8cd8ea8973c22b36d
Jon Earley <jonathan.a.earley@gmail.com>
2020-03-30 16:11:24 -0400

    Add COVID alert for Everything and Catalog.

--- src/modules/core/components/SearchHeader/index.js
+++ src/modules/core/components/SearchHeader/index.js
@@ -1,52 +1,61 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import Header from './header'
-import config from '../../../../config'
+import React from "react";
+import { connect } from "react-redux";
+import Header from "./header";
+import config from "../../../../config";
 
 class SearchHeader extends React.Component {
   render() {
     const loginRoot = config.loginUrl;
-    const loginUrl = loginRoot + '?dest=' + encodeURIComponent(document.location.pathname + document.location.search)
-    const logoutUrl = 'https://weblogin.umich.edu/cgi-bin/logout?' + document.location
-    const loginText = this.props.isAuthenticated ? 'Log out' : 'Log in'
-    const loginHref = this.props.isAuthenticated ? logoutUrl : loginUrl
+    const loginUrl =
+      loginRoot +
+      "?dest=" +
+      encodeURIComponent(document.location.pathname + document.location.search);
+    const logoutUrl =
+      "https://weblogin.umich.edu/cgi-bin/logout?" + document.location;
+    const loginText = this.props.isAuthenticated ? "Log out" : "Log in";
+    const loginHref = this.props.isAuthenticated ? logoutUrl : loginUrl;
     let navItems = [
-      { text: 'My Account', href: 'https://www.lib.umich.edu/my-account/' },
-      { text: 'My Favorites', href: 'https://www.lib.umich.edu/my-account/favorites' },
+      { text: "My Account", href: "https://www.lib.umich.edu/my-account/" },
+      {
+        text: "My Favorites",
+        href: "https://www.lib.umich.edu/my-account/favorites"
+      },
       { text: loginText, href: loginHref }
-    ]
+    ];
 
     if (this.props.favoritesDisabled) {
       navItems = [
-        { text: 'My Account', href: 'https://www.lib.umich.edu/my-account/' },
+        { text: "My Account", href: "https://www.lib.umich.edu/my-account/" },
         { text: loginText, href: loginHref }
-      ]
+      ];
     }
 
-    return  (
+    return (
       <div role="banner">
         <Header
           name="Search"
           className="search-header"
           siteUrl="/everything"
           nav={navItems}
-          renderAnchor={(data) => (
+          renderAnchor={data => (
             <a
               href={data.to}
               data-ga-action="Click"
               data-ga-category="Header"
               data-ga-label={data.text}
-            >{data.text}</a>
+            >
+              {data.text}
+            </a>
           )}
         />
       </div>
     );
   }
-};
+}
 
 function mapStateToProps(state) {
   return {
-    isAuthenticated: state.profile.status === 'Logged in',
+    isAuthenticated: state.profile.status === "Logged in",
     datastore: state.datastores.active,
     search: state.search,
     favoritesDisabled: state.favorites.disabled === true
--- src/modules/pages/components/DatastorePage/container.js
+++ src/modules/pages/components/DatastorePage/container.js
@@ -1,95 +1,65 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/core'
-import React from 'react'
-import { connect } from 'react-redux'
-import MediaQuery from 'react-responsive';
-import { _ } from 'underscore'
-import {
-  Route,
-  Switch
-} from 'react-router-dom'
+import { jsx } from "@emotion/core";
+import React from "react";
+import { connect } from "react-redux";
+import MediaQuery from "react-responsive";
+import { _ } from "underscore";
+import { Route, Switch } from "react-router-dom";
 
-import {
-  NoMatch
-} from '../../../pages'
+import { NoMatch } from "../../../pages";
 
-import {
-  SearchBox,
-} from '../../../search'
+import { SearchBox } from "../../../search";
 
-import {
-  AdvancedSearch
-} from '../../../advanced'
+import { AdvancedSearch } from "../../../advanced";
 
 import {
   DatastoreNavigation,
   DatastoreInfo,
   Landing,
-  DatastoreAuthenticationAlert
-} from '../../../datastores'
+  DatastoreAuthenticationAlert,
+  DatastoreAlerts
+} from "../../../datastores";
 
-import {
-  Filters
-} from '../../../filters'
+import { Filters } from "../../../filters";
 
-import {
-  BrowsePage,
-  BrowseInfo
-} from '../../../browse'
+import { BrowsePage, BrowseInfo } from "../../../browse";
 
 import {
   RecordList,
   Pagination,
   BentoboxList,
   RecordFull
-} from '../../../records'
+} from "../../../records";
 
-import {
-  GetThisPage
-} from '../../../getthis'
+import { GetThisPage } from "../../../getthis";
 
-import {
-  switchPrideToDatastore
-} from '../../../pride'
+import { switchPrideToDatastore } from "../../../pride";
 
-import {
-  InstitutionSelect,
-  InstitutionWrapper
-} from '../../../institution'
+import { InstitutionSelect, InstitutionWrapper } from "../../../institution";
 
-import {
-  List
-} from '../../../lists'
+import { List } from "../../../lists";
 
-import {
-  setDocumentTitle
-} from '../../../a11y'
+import { setDocumentTitle } from "../../../a11y";
 
-import {
-  FlintAlerts
-} from '../../../flint'
-
+import { FlintAlerts } from "../../../flint";
 
 const ConnectedSwitch = connect(mapStateToProps)(Switch);
 
 class DatastorePageContainer extends React.Component {
   componentWillMount() {
     // Switch Pride to the appropriate datastore
-    const { datastoreSlug } = this.props.match.params
-    switchPrideToDatastore(datastoreSlug)
+    const { datastoreSlug } = this.props.match.params;
+    switchPrideToDatastore(datastoreSlug);
   }
 
   componentDidUpdate() {
-    const {
-      activeDatastore,
-      query
-    } = this.props;
+    const { activeDatastore, query } = this.props;
 
     if (activeDatastore) {
       if (query) {
-        setDocumentTitle([query, activeDatastore.name])
+        setDocumentTitle([query, activeDatastore.name]);
       } else {
-        setDocumentTitle([activeDatastore.name])
+        setDocumentTitle([activeDatastore.name]);
       }
     }
   }
@@ -105,100 +75,131 @@ class DatastorePageContainer extends React.Component {
     } = this.props;
 
     if (activeDatastore === undefined) {
-      return null
+      return null;
     }
 
     return (
       <main className="main-container">
         <Switch>
-          <Route path={`/:datastoreSlug/browse`} location={location} render={() => {
-            if (activeDatastore.uid === 'databases' || activeDatastore.uid === 'journals') {
-              return (
-                <BrowsePage />
-              )
-            }
+          <Route
+            path={`/:datastoreSlug/browse`}
+            location={location}
+            render={() => {
+              if (
+                activeDatastore.uid === "databases" ||
+                activeDatastore.uid === "journals"
+              ) {
+                return <BrowsePage />;
+              }
 
-            return <NoMatch/>
-          }}/>
-          <Route path={`/:datastoreSlug/advanced`} location={location} render={() => {
-            if (isAdvanced) {
-              return (
-                <AdvancedSearch
-                  handleBasicSearchQueryChange={this.handleChange}
-                  searchQueryFromURL={location.search}
-                />
-              )
-            }
+              return <NoMatch />;
+            }}
+          />
+          <Route
+            path={`/:datastoreSlug/advanced`}
+            location={location}
+            render={() => {
+              if (isAdvanced) {
+                return (
+                  <AdvancedSearch
+                    handleBasicSearchQueryChange={this.handleChange}
+                    searchQueryFromURL={location.search}
+                  />
+                );
+              }
 
-            return <NoMatch/>
-          }}/>
-          <Route path={`/:datastoreSlug`} location={location} render={() => (
-            <React.Fragment>
-              <SearchBox />
-              <DatastoreNavigation />
-              <div css={{
-                marginTop: "-0.75rem",
-                '.alert-inner': {
-                  display: 'flex',
-                  justifyContent: 'center'
-                },
-                ':empty': {
-                  display: 'none'
-                }
-              }}>
-                <FlintAlerts />
-              </div>
-              <DatastoreAuthenticationAlert />
-              <ConnectedSwitch>
-                <Route path={match.url + `/record/:recordUid/get-this/:barcode`} render={(props) => {
-                  return (
-                    <GetThisPage />
-                  )
-                }}/>
-                <Route path={match.url + `/record/:recordUid`} exact render={(props) => {
-                  return (
-                    <RecordFull />
-                  )
-                }}/>
-                <Route path={match.url + `/list`} exact render={(props) => {
-                  return (
-                    <List />
-                  )
-                }}/>
-                <Route match={match.url} render={() => {
-                  return (
-                    <InstitutionWrapper>
-                      {!searching ? (
-                        <div className="container">
-                          <Landing activeDatastore={activeDatastore} />
-                        </div>
-                      ) : (
-                        <DatastoreInfo activeDatastore={activeDatastore} />
-                      )}
-                      <Results searching={searching} activeDatastore={activeDatastore} activeFilterCount={activeFilterCount} />
-                    </InstitutionWrapper>
-                  )
-                }}/>
-              </ConnectedSwitch>
-            </React.Fragment>
-          )}/>
+              return <NoMatch />;
+            }}
+          />
+          <Route
+            path={`/:datastoreSlug`}
+            location={location}
+            render={() => (
+              <React.Fragment>
+                <SearchBox />
+                <DatastoreNavigation />
+                <div
+                  css={{
+                    marginTop: "-0.75rem",
+                    ".alert-inner": {
+                      display: "flex",
+                      justifyContent: "center"
+                    },
+                    ":empty": {
+                      display: "none"
+                    }
+                  }}
+                >
+                  <DatastoreAlerts />
+                  <FlintAlerts />
+                </div>
+                <DatastoreAuthenticationAlert />
+                <ConnectedSwitch>
+                  <Route
+                    path={match.url + `/record/:recordUid/get-this/:barcode`}
+                    render={props => {
+                      return <GetThisPage />;
+                    }}
+                  />
+                  <Route
+                    path={match.url + `/record/:recordUid`}
+                    exact
+                    render={props => {
+                      return <RecordFull />;
+                    }}
+                  />
+                  <Route
+                    path={match.url + `/list`}
+                    exact
+                    render={props => {
+                      return <List />;
+                    }}
+                  />
+                  <Route
+                    match={match.url}
+                    render={() => {
+                      return (
+                        <InstitutionWrapper>
+                          {!searching ? (
+                            <div className="container">
+                              <Landing activeDatastore={activeDatastore} />
+                            </div>
+                          ) : (
+                            <DatastoreInfo activeDatastore={activeDatastore} />
+                          )}
+                          <Results
+                            searching={searching}
+                            activeDatastore={activeDatastore}
+                            activeFilterCount={activeFilterCount}
+                          />
+                        </InstitutionWrapper>
+                      );
+                    }}
+                  />
+                </ConnectedSwitch>
+              </React.Fragment>
+            )}
+          />
         </Switch>
       </main>
-    )
+    );
   }
 }
 
 const Results = ({ searching, activeDatastore, activeFilterCount }) => {
   if (activeDatastore.isMultisearch && searching) {
-    return <MultisearchSearching activeDatastore={activeDatastore}/>
+    return <MultisearchSearching activeDatastore={activeDatastore} />;
   }
 
   return (
-    <div className="container container-medium flex-container" style={{ marginTop: '0.75rem' }}>
+    <div
+      className="container container-medium flex-container"
+      style={{ marginTop: "0.75rem" }}
+    >
       {!activeDatastore.isMultisearch ? (
         <div className="side-container">
           <MediaQuery minWidth={980}>
-            {(matches) => {
+            {matches => {
               if (matches) {
                 return (
                   <React.Fragment>
@@ -210,14 +211,19 @@ const Results = ({ searching, activeDatastore, activeFilterCount }) => {
                       </React.Fragment>
                     ) : null}
                   </React.Fragment>
-                )
+                );
               } else {
-                const hasActiveFilters = activeFilterCount > 0
-                const summaryClassName = hasActiveFilters ? "small-screen-filter-summary small-screen-filter-summary--active-filters" : "small-screen-filter-summary"
+                const hasActiveFilters = activeFilterCount > 0;
+                const summaryClassName = hasActiveFilters
+                  ? "small-screen-filter-summary small-screen-filter-summary--active-filters"
+                  : "small-screen-filter-summary";
 
                 return (
                   <details className="small-screen-filter-details">
-                    <summary className={summaryClassName}>Filters {hasActiveFilters ? (`(${activeFilterCount})`) : null}</summary>
+                    <summary className={summaryClassName}>
+                      Filters{" "}
+                      {hasActiveFilters ? `(${activeFilterCount})` : null}
+                    </summary>
 
                     {searching ? (
                       <React.Fragment>
@@ -227,39 +233,41 @@ const Results = ({ searching, activeDatastore, activeFilterCount }) => {
                       </React.Fragment>
                     ) : null}
                   </details>
-                )
+                );
               }
             }}
           </MediaQuery>
         </div>
-      ) : null }
+      ) : null}
 
       {searching ? (
         <div className="results-container">
           <RecordList />
           <Pagination />
         </div>
-      ) : null }
+      ) : null}
     </div>
-  )
-}
+  );
+};
 
 const MultisearchSearching = () => {
   return (
     <div className="container container-large flex-container">
       <BentoboxList />
     </div>
-  )
-}
+  );
+};
 
 function mapStateToProps(state) {
-  const activeFilters = state.filters.active[state.datastores.active]
+  const activeFilters = state.filters.active[state.datastores.active];
 
   return {
     searching: state.search.searching,
     query: state.search.query,
     datastores: state.datastores,
-    activeDatastore: _.findWhere(state.datastores.datastores, { uid: state.datastores.active }),
+    activeDatastore: _.findWhere(state.datastores.datastores, {
+      uid: state.datastores.active
+    }),
     location: state.router.location,
     isAdvanced: state.advanced[state.datastores.active] ? true : false,
     activeFilterCount: activeFilters ? Object.keys(activeFilters).length : 0
