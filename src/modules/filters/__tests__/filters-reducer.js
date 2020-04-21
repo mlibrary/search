c5ccba0cdafb386b4854f0c057b6230dea4736a2
Albert Bertram <bertrama@umich.edu>
2020-04-21 14:03:17 -0400

    Add filter reducers test for the degenerate case.

---
+++ src/modules/filters/__tests__/filters-reducer.js
@@ -0,0 +1,10 @@
+import filtersReducer from '../reducer'
+
+it('reduces filters', () => {
+  const state = {}
+  const action = {}
+  const result = {}
+
+  expect(filtersReducer(state, action)).toEqual(result);
+});
+
