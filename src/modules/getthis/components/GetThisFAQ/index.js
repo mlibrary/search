05279f5f0503038679610a0d843643c8b14ad67d
Bridget Burke <burkebri@umich.edu>
2020-11-09 11:01:59 -0500

    Updating Get This FAQ text.

--- src/modules/getthis/components/GetThisFAQ/index.js
+++ src/modules/getthis/components/GetThisFAQ/index.js
@@ -5,8 +5,12 @@ const GetThisFAQ = () => (
   <section className="card get-this-section">
     <Heading size="medium" level={3} style={{ marginTop: 0 }}>Frequently asked questions</Heading>
 
-    <Heading size="small" level={4}>Contactless pickup isn’t listed as an option for some physical items. Instead I see “Full text available online” as part of HathiTrust. Why is that?</Heading>
-    <p className="u-margin-top-none">Items available through HathiTrust Emergency Temporary Access (ETAS) are not available for contactless pickup at this time due to the ETAS terms of services related to copyrighted works.</p>
+    <Heading size="small" level={4}>When can I request a physical copy for contactless pickup?</Heading>
+    <p className="u-margin-top-none">Many items in our physical collection have been digitized and made available through the HathiTrust Digital Library. When this is the case, you'll see “Full text available online."</p>
+
+    <p className="u-margin-top-none">For physical items from our collection that aren't digitized and available through HathiTrust, you'll see an option for "Contactless pickup at the library."</p>
+
+    <p className="u-margin-top-none">Due to copyright limitations, we cannot check out multiple copies of an item that has been digitized, which means the HathiTrust version may be the only option available.</p>
 
   </section>
 )
