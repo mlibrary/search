1a7aa71e6a9a59c60049345543b37afdb48d9218
Bridget Burke <burkebri@umich.edu>
2020-12-01 19:28:37 -0500

    SEARCH-1239 Add target attribute to link

--- src/modules/core/components/Feedback/index.js
+++ src/modules/core/components/Feedback/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Icon } from "../../../reusable";
 
 const Feedback = () => {
   return (
@@ -6,8 +7,17 @@ const Feedback = () => {
       <a
         href="https://umich.qualtrics.com/jfe/form/SV_bCwYIKueEXs8wBf"
         className="feedback-qualtrics-link"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Give feedback about this page - Opens in new window"
+        style={{padding:".15em"}}
       >
         Give feedback about this page
+         <Icon
+          icon="open_in_new"
+          size={22}
+          style={{paddingLeft: ".25em"}}
+        />
       </a>
     </aside>
   );
