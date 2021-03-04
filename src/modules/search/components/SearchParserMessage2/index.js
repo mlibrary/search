aa2105577a37cb22a7a6af0250ec46e76e10e671
Jon Earley <jonathan.a.earley@gmail.com>
2021-03-04 10:53:23 -0500

    Clear parser message when running a search

--- src/modules/search/components/SearchParserMessage2/index.js
+++ src/modules/search/components/SearchParserMessage2/index.js
@@ -13,60 +13,55 @@ export default function SearchParserMessage2() {
     return null;
   }
 
-  const handleDismiss = () => {
-    store.dispatch(setParserMessage(null));
-  };
-
   return (
     <section
-        className="parser-message"
-          css={{
-            width: `100%`,
-            color: "#333"
-          }}
-        >
-        <p>
-          <strong
-            css={{
-             fontWeight: "600",
-              color: "#333"
-          }}
-          >
-          Showing results for:{" "}
-          </strong>
-          <strong
+      className="parser-message"
+      css={{
+        width: `100%`,
+        color: "#333",
+      }}
+    >
+      <p>
+        <strong
           css={{
             fontWeight: "600",
-             color: "#0C5292"
-           }}>
-           {parserMessage.details}
-            </strong>
-        </p>
-        
+            color: "#333",
+          }}
+        >
+          Showing results for:{" "}
+        </strong>
+        <strong
+          css={{
+            fontWeight: "600",
+            color: "#0C5292",
+          }}
+        >
+          {parserMessage.details}
+        </strong>
+      </p>
+
       <p>
         You searched for:{" "}
         <strong
-            css={{
+          css={{
             fontWeight: "600",
-              fontSize: "1em"
-              }}>
+            fontSize: "1em",
+          }}
+        >
           {parserMessage.class}
         </strong>
-              
-            </p>
-        
-        <p>
-            <strong
-                css={{
-                  fontWeight: "600",
-                  color: "#AA5600"
-                }}
+      </p>
+
+      <p>
+        <strong
+          css={{
+            fontWeight: "600",
+            color: "#AA5600",
+          }}
         >
-           <Icon icon="error" size={20} />
-          {" "}Summary:{" "}
-          {parserMessage.summary}
-              </strong>
-            </p>
-      </section>
+          <Icon icon="error" size={20} /> Summary: {parserMessage.summary}
+        </strong>
+      </p>
+    </section>
   );
 }
