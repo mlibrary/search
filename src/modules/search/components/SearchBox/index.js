52f7aa9df64ed4e6701b69b0c609ddd982ca6a98
Jon Earley <Jonathan.a.earley@gmail.com>
2021-10-27 12:21:49 -0400

    Beginning of rewriting the basic search box to include a field.

--- src/modules/search/components/SearchBox/index.js
+++ src/modules/search/components/SearchBox/index.js
@@ -1,161 +1,75 @@
-import React from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import React from 'react'
+import {
+  COLORS,
+  SPACING,
+  Button,
+  Icon
+} from '@umich-lib/core'
+import { useSelector } from "react-redux";
 import qs from "qs";
-import { withRouter, Link } from "react-router-dom";
-import _ from "underscore";
 
-import { setSearchQueryInput, searching } from "../../actions";
-import { Icon } from "../../../core";
-import ReactGA from "react-ga";
+function SearchBox() {
+  const { query } = useSelector((state) => state.search);
+  const { fields } = useSelector((state) => state.advanced[state.datastores.active]);
+  const [inputQuery, setInputQuery] = React.useState(query)
+  const [field, setField] = React.useState()
 
-class SearchBox extends React.Component {
-  constructor(props) {
-    super(props);
+  function handleSubmitSearch(e) {
+    e.preventDefault()
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.onBackButtonEvent = this.onBackButtonEvent.bind(this);
+    const newQuery =  field ? `${field}:(${inputQuery})` : inputQuery
+
+    console.log('query', query)
+    console.log('newQuery', newQuery)
+
+    if (query === newQuery) return // nothing new here
+
+    console.log('new!')
+
+    const newURL = qs.stringify({
+      // preserve existing URL state
+      ...qs.parse(document.location.search.substring(1), { allowDots: true }),
+      
+      // and add new query
+      query: newQuery
+    }, {
+      arrayFormat: "repeat",
+      encodeValuesOnly: true,
+      allowDots: true,
+      format: "RFC1738"
+    })
+
+    console.log('newURL', newURL)
   }
 
-  handleChange(query) {
-    this.props.setSearchQueryInput(query);
-  }
-
-  onBackButtonEvent(e) {
-    const { query } = this.props;
-    this.handleChange(query);
-  }
-
-  componentDidMount() {
-    window.onpopstate = this.onBackButtonEvent;
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    const {
-      match,
-      history,
-      queryInput,
-      activeFilters,
-      institution,
-      sort,
-      activeDatastore,
-    } = this.props;
-
-    ReactGA.event({
-      action: "Click",
-      category: "Search Button",
-      label: `Search ${activeDatastore.name}`,
-    });
-
-    const library =
-      activeDatastore.uid === "mirlyn" ? institution.active : undefined;
-
-    // Query is not empty
-    if (queryInput.length > 0) {
-      const queryString = qs.stringify(
-        {
-          query: queryInput,
-          filter: activeFilters,
-          library,
-          sort,
-        },
-        {
-          arrayFormat: "repeat",
-          encodeValuesOnly: true,
-          allowDots: true,
-          format: "RFC1738",
-        }
-      );
-
-      const url = `/${match.params.datastoreSlug}?${queryString}`;
-
-      history.push(url);
-    }
-  }
-
-  render() {
-    const {
-      match,
-      location,
-      queryInput,
-      isAdvanced,
-      activeDatastore,
-    } = this.props;
-
-    return (
-      <div className="search-box-container-full">
-        <div className="search-box-container">
-          <form
-            className="search-box-form"
-            onSubmit={this.handleSubmit}
-            role="search"
-            id="search-box"
-          >
-            <div className="search-box">
-              <input
-                id="search-query"
-                className="search-box-input"
-                type="search"
-                aria-label="search text"
-                value={queryInput}
-                spellCheck="false"
-                data-hj-allow
-                onChange={(event) => this.handleChange(event.target.value)}
-              />
-              <button className="button search-box-button" type="submit">
-                <Icon name="search" />
-                <span className="search-box-button-text">Search</span>
-              </button>
-            </div>
-
-            {isAdvanced && (
-              <div className="search-box-advanced">
-                <Link
-                  to={`/${match.params.datastoreSlug}/advanced${location.search}`}
-                  className="search-box-advanced-link"
-                >
-                  <span className="offpage">{activeDatastore.name}</span>
-                  <span>Advanced</span>
-                  <span className="offpage">Search</span>
-                </Link>
-              </div>
-            )}
-          </form>
+  return (
+    <form css={{
+      background: COLORS.blue['300'],
+      padding: `1rem 0`,
+      borderBottom: `solid 2px ${COLORS.blue['400']}`
+    }} onSubmit={handleSubmitSearch}>
+      <div class="container container-medium">
+        <div css={{
+          display: 'grid',
+          gridTemplateColumns: 'auto 1fr auto',
+          gap: '0.5rem'
+        }}>
+          <select class="dropdown" onChange={e => setField(e.target.value)}>
+            {fields.map(field => <option value={field.uid}>{field.name}</option>)}
+          </select>
+          <input type="text" value={inputQuery} onChange={e => setInputQuery(e.target.value)}></input>
+          <Button css={{
+            display: 'flex',
+            alignItems: 'center',
+          }}><Icon icon="search" css={{
+            marginRight: '0.25rem'
+          }} />Search</Button>
         </div>
       </div>
-    );
-  }
+    </form>
+  )
 }
 
-function mapStateToProps(state) {
-  return {
-    isSearching: state.search.searching,
-    query: state.search.query,
-    queryInput: state.search.queryInput,
-    activeFilters: state.filters.active[state.datastores.active],
-    activeDatastore: _.findWhere(state.datastores.datastores, {
-      uid: state.datastores.active,
-    }),
-    location: state.router.location,
-    isAdvanced: state.advanced[state.datastores.active] ? true : false,
-    institution: state.institution,
-    datastores: state.datastores,
-    sort: state.search.sort[state.datastores.active],
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators(
-    {
-      setSearchQueryInput,
-      searching,
-    },
-    dispatch
-  );
-}
-
-export default withRouter(
-  connect(mapStateToProps, mapDispatchToProps)(SearchBox)
-);
+export default SearchBox
