e969a5d4ae6cd228d0bbf26ab8b0a31c95a2562e
Jonathan Earley <yelrae@Jonathans-MacBook-Air.local>
2021-11-10 11:48:56 -0500

    Add browse by LC call number

--- src/modules/search/components/SearchBox/index.js
+++ src/modules/search/components/SearchBox/index.js
@@ -120,6 +120,7 @@ function SearchBox({ history, match, location }) {
               }}
             >
               {fields.map(field => <option value={field.uid}>{field.name}</option>)}
+              {activeDatastore.uid === 'mirlyn' && (<option value='browse_by_lc_callnumber'>Browse by LC call number</option>)}
             </select>
               <Icon d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" size={24} css={{
                 position: 'absolute',
