def5f6dc1636cfe029ee35e569b9a46c8015aa50
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-12-13 11:18:12 -0500

    SEARCH-1538 Changes

--- src/modules/search/components/SearchBox/index.js
+++ src/modules/search/components/SearchBox/index.js
@@ -95,7 +95,7 @@ function SearchBox({ history, match, location }) {
             position: 'relative',
             width: '100%',
             [MEDIA_QUERIES.LARGESCREEN]: {
-              maxWidth: '280px'
+              maxWidth: '340px'
             }
           }}>
             <select
@@ -125,7 +125,7 @@ function SearchBox({ history, match, location }) {
                     {fields.map(field => <option value={field.uid}>{field.name}</option>)}
                   </optgroup>
                   <optgroup label={`Browse by`}>
-                    <option value='browse_by_lc_callnumber'>Browse by LC call number</option>
+                    <option value='browse_by_callnumber'>Browse by call number (LC and Dewey)</option>
                     <option value='browse_by_author' disabled>Browse by author (coming soon)</option>
                     <option value='browse_by_subject' disabled>Browse by subject (coming soon)</option>
                     <option value='browse_by_title' disabled>Browse by title (coming soon)</option>
