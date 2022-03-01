507ed2f65beaa269444e63fe1634745efda75a54
Erin E. Sullivan <sulliv.erin@gmail.com>
2022-03-01 14:42:17 -0500

    Removing GTM event

--- src/modules/favorites/components/FavoriteRecord/index.js
+++ src/modules/favorites/components/FavoriteRecord/index.js
@@ -29,30 +29,6 @@ class FavoriteRecord extends React.Component {
     if (!login.authenticated) {
       this.setState({ promptLogin: true })
     } else {
-      /*
-        Google Analytics wants to know if this
-        button is clicked on the full page or
-        from results.
-
-        So the component is wrapped with withRouter()
-        to provide router information in props. We check
-        if this component is rendered from results by
-        checking the match props.
-
-        Because favorite button only appears on medium
-        or full records, we can assume it's full if not
-        matching the results view.
-      */
-
-      let ga_label = match.path === '/:datastoreSlug' ? 'medium' : 'full'
-      let ga_add_or_remove = isFavorited === true ? 'remove' : 'save'
-
-      ReactGA.event({
-        action: 'Click',
-        category: 'Favorites',
-        label: `${ga_add_or_remove} favorite item from ${datastore.name} ${ga_label} record`
-      })
-
       const data = {
         intent: isFavorited ? 'unfavorite' : 'favorite',
         datastore: datastore.uid,
