375df2f84ebe8e39e405da874580294711e740f3
Erin E. Sullivan <sulliv.erin@gmail.com>
2023-04-14 11:22:43 -0400

    Updating `history` import and simplifying code.

--- src/history.js
+++ src/history.js
@@ -1,6 +1,6 @@
-const createHistory = require("history").createBrowserHistory;
+import { createBrowserHistory } from 'history';
 
 // Create a history of your choosing (we're using a browser history in this case)
-const history = createHistory();
+const history = createBrowserHistory();
 
 export default history;
