39509fd3ff9fc54cabdda78898ffbd88d8c4a5ab
Erin E. Sullivan <sulliv.erin@gmail.com>
2023-05-01 09:11:45 -0400

    Reverting accidental changes.

--- src/modules/a11y/setDocumentTitle.js
+++ src/modules/a11y/setDocumentTitle.js
@@ -1,8 +1,13 @@
 import store from './../../store';
-import { setA11yMessage } from './';
+import {
+  setA11yMessage
+} from './';
 
-export default function setDocumentTitle (titles) {
+const setDocumentTitle = (titles) => {
   const documentTitle = titles.concat('Library Search').join(' - ');
+
   store.dispatch(setA11yMessage(documentTitle));
   document.title = documentTitle;
 };
+
+export default setDocumentTitle;
