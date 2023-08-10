f92aa8dae72ebcef16c3c520c23e93c9b1b30ecd
Erin E. Sullivan <sulliv.erin@gmail.com>
2023-08-10 14:10:34 -0400

    Setting state and effect. - Sets default state to first object key. - Sets default state if url parameter `affiliation` exists, and sets `localStorage`. - Sets default state if `localStorage` exists.

--- src/modules/affiliation/components/ChooseAffiliation/index.js
+++ src/modules/affiliation/components/ChooseAffiliation/index.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import { React, useState } from 'react';
 
 export default function ChooseAffiliation () {
+  const affiliations = {
+    aa: 'Ann Arbor',
+    flint: 'Flint'
+  };
+  let defaultAffiliation = Object.keys(affiliations)[0];
+  // Set default affiliation if query parameter exists
+  const urlParams = new URLSearchParams(window.location.search);
+  const affiliationParam = urlParams.get('affiliation');
+  if (affiliationParam) {
+    defaultAffiliation = affiliationParam;
+    // Set localStorage
+    localStorage.setItem('affiliation', affiliationParam);
+  }
+  const affiliationLocalStorage = localStorage.getItem('affiliation');
+  // Set default affiliation if localStorage exists
+  if (affiliationLocalStorage) {
+    defaultAffiliation = affiliationLocalStorage;
+  }
+  const [affiliation, setAffiliation] = useState(defaultAffiliation);
+  const onAffiliationChange = (event) => {
+    const value = event.target.value;
+    setAffiliation(value);
+  };
+
   return (
     <fieldset className='institution-select-container'>
       <legend className='visually-hidden'>Choose Affiliation</legend>
@@ -14,13 +38,16 @@ export default function ChooseAffiliation () {
         className='dropdown'
         id='choose-affiliation'
         autoComplete='off'
+        value={affiliation}
+        onChange={onAffiliationChange}
       >
-        <option value='aa'>
-          Ann Arbor
-        </option>
-        <option value='flint'>
-          Flint
-        </option>
+        {Object.keys(affiliations).map((affiliation) => {
+          return (
+            <option value={affiliation} key={`affiliation-${affiliation}`}>
+              {affiliations[affiliation]}
+            </option>
+          );
+        })}
       </select>
     </fieldset>
   );
