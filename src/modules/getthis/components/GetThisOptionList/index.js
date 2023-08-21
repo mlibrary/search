45c7c5e43afedce7fccb632f30ddae605dea2f18
Erin E. Sullivan <sulliv.erin@gmail.com>
2023-08-21 10:27:37 -0400

    Converting `GetThisOptionList` back to functional component. Simplifying to remove `Section` component.

--- src/modules/getthis/components/GetThisOptionList/index.js
+++ src/modules/getthis/components/GetThisOptionList/index.js
@@ -1,85 +1,64 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Icon, Alert } from '../../../reusable';
 import { DetailsList } from '../../../core';
 import GetThisOption from '../GetThisOption';
 import { Authentication } from '../../../profile';
 import PropTypes from 'prop-types';
 
-const Section = ({ children }) => {
-  return (
-    <section className='card get-this-section y-spacing'>
-      <h2 className='heading-large' style={{ marginTop: '0' }}>How would you like to get this item?</h2>
-      {children}
-    </section>
+function GetThisOptions (props) {
+  let section = (
+    <div className='alert'>
+      <p>Loading holding options...</p>
+    </div>
   );
-};
 
-Section.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
-  ])
-};
+  if (props.record?.getthis) {
+    const { status, options } = props.record.getthis;
 
-class GetThisOptions extends React.Component {
-  render () {
-    const { record } = this.props;
+    section = (
+      <div className='alert'>
+        <p>Sorry, something unexpected happened.</p>
+        <p><span className='strong'>Status:</span> {status}</p>
+      </div>
+    );
 
-    if (record && record.getthis) {
-      const { status, options } = record.getthis;
+    if (status === 'Success') {
+      section = (
+        <div className='alert alert--error'>
+          No options available.
+        </div>
+      );
 
-      if (status === 'Success') {
-        if (options.length === 0) {
-          return (
-            <Section>
-              <Alert type='error'>
-                <div className='x-spacing'><Icon icon='error' /><span>No options available.</span></div>
-              </Alert>
-            </Section>
-          );
-        }
-
-        return (
-          <Section>
-            <DetailsList className='get-this-options'>
-              {options.map((option, key) => {
-                return (
-                  <GetThisOption option={option} key={key} />
-                );
-              })}
-            </DetailsList>
-          </Section>
-        );
-      } else if (status === 'Not logged in') {
-        return (
-          <Section>
-            <Authentication button>
-              <span className='strong'>Log in</span> to view request options
-            </Authentication>
-          </Section>
-        );
-      } else {
-        return (
-          <Section>
-            <div className='alert'>
-              <p>Sorry, something unexpected happened.</p>
-
-              <p><span className='strong'>Status:</span> {status}</p>
-            </div>
-          </Section>
+      if (options.length) {
+        section = (
+          <DetailsList className='get-this-options'>
+            {options.map((option, key) => {
+              return (
+                <GetThisOption option={option} key={key} />
+              );
+            })}
+          </DetailsList>
         );
       }
-    } else {
-      return (
-        <Section>
-          <div className='alert'>
-            <p>Loading holding options...</p>
-          </div>
-        </Section>
+    }
+
+    if (status === 'Not logged in') {
+      section = (
+        <Authentication button>
+          <span className='strong'>Log in</span> to view request options
+        </Authentication>
       );
     }
   }
+
+  return (
+    <section className='card get-this-section y-spacing'>
+      <h2 className='heading-large' style={{ marginTop: '0' }}>
+        How would you like to get this item?
+      </h2>
+      {section}
+    </section>
+  );
 }
 
 GetThisOptions.propTypes = {
