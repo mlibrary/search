ccaffdaf75b76fdc0b488e3234342729f1471a04
Erin E. Sullivan <sulliv.erin@gmail.com>
2024-02-29 14:01:14 -0500

    Simplifying `Alert` component.

--- src/modules/reusable/components/Alert/index.js
+++ src/modules/reusable/components/Alert/index.js
@@ -1,2 +1,27 @@
-import Alert from './Alert';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Alert ({ type = 'informational', children }) {
+  return (
+    <div className={`alert alert--${type}`}>
+      <div className='alert-inner font-small x-spacing'>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+Alert.propTypes = {
+  type: PropTypes.oneOf([
+    'informational',
+    'error',
+    'warning',
+    'success'
+  ]),
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ])
+};
+
 export default Alert;
