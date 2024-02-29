a962f2d07ca2324ed9154795f7eab6c29a02feea
Erin E. Sullivan <sulliv.erin@gmail.com>
2024-02-29 11:33:44 -0500

    Simplifying `handleGAClick`.

--- src/modules/analytics/handleGAClick.js
+++ src/modules/analytics/handleGAClick.js
@@ -1,27 +1,16 @@
 import ReactGA from 'react-ga';
 
-/*
-  Send Google Analytic Events by capturing event propogation
-  where targets have action, category, and label attrubutes.
-*/
 const handleGAClick = (e) => {
-  // if (process.env.NODE_ENV === 'production') {
-  const action = e.target.getAttribute('data-ga-action');
-  const category = e.target.getAttribute('data-ga-category');
-  const label = e.target.getAttribute('data-ga-label');
+  const event = {};
+  ['action', 'category', 'label'].forEach((property) => {
+    event[property] = e.target.getAttribute(`data-ga-${property}`);
+  });
 
-  // Only send events when target is has the three attributes.
-  if (action && category && label) {
-    const event = {
-      action,
-      category,
-      label
-    };
-
-    // Send the event to Google Analytics
+  if (Object.keys(event).every((property) => {
+    return !!property;
+  })) {
     ReactGA.event(event);
   }
-  // }
 };
 
 export default handleGAClick;
