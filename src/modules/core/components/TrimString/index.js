0b19428ea1f4f5f19711d73cf543b7cc40b111d3
Erin E. Sullivan <sulliv.erin@gmail.com>
2024-02-29 10:45:48 -0500

    Converting `TrimString` to a functional component.

--- src/modules/core/components/TrimString/index.js
+++ src/modules/core/components/TrimString/index.js
@@ -1,67 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-class TrimString extends React.Component {
-  constructor (props) {
-    super(props);
+function TrimString ({ string, expandable, trimLength = 240 }) {
+  const [show, setShow] = useState(false);
 
-    this.state = {
-      show: false,
-      trimLength: this.props.trimLength || 240,
-      showMore: this.props.showMore !== false
-    };
+  const handleShowToggleClick = () => {
+    setShow(!show);
+  };
 
-    this.handleShowToggleClick = this.handleShowToggleClick.bind(this);
+  if (!string) {
+    return null;
   }
 
-  handleShowToggleClick () {
-    this.setState({
-      show: !this.state.show
-    });
-  }
-
-  render () {
-    const { string } = this.props;
-
-    if (string?.length < this.state.trimLength) {
-      return string;
-    }
-
-    let displayString = null;
-    if (this.state.show) {
-      displayString = string;
-    } else {
-      displayString = `${string.substr(0, this.state.trimLength)}...`;
-    }
-
-    if (!this.state.showMore) {
-      return (
-        <span className='trim-string-text'>{displayString}</span>
-      );
-    }
-
-    return (
-      <span className='trim-string-expandable'>
-        <span className='trim-string-text'>{displayString}</span>
-        {this.props.expandable && (
-          <button
-            onClick={() => {
-              return this.handleShowToggleClick();
-            }}
-            className='trim-string-button'
-          >
-            {this.state.show ? 'Show less' : 'Show more'}
-          </button>
-        )}
-      </span>
-    );
-  }
+  return (
+    <>
+      {(string.length < trimLength || show) ? string : `${string.substr(0, trimLength)}...`}
+      {expandable && (
+        <button onClick={handleShowToggleClick} className='btn btn--secondary font-small trim-string-button'>
+          {`Show ${show ? 'less' : 'more'}`}
+        </button>
+      )}
+    </>
+  );
 }
 
 TrimString.propTypes = {
+  string: PropTypes.string.isRequired,
   trimLength: PropTypes.number,
-  string: PropTypes.string,
-  showMore: PropTypes.bool,
   expandable: PropTypes.bool
 };
 
