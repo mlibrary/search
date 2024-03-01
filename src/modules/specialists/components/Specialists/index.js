08237230d522aa26125b09943dd188a8a0eafd35
Erin E. Sullivan <sulliv.erin@gmail.com>
2024-03-01 11:19:48 -0500

    Simplifying syntax.

--- src/modules/specialists/components/Specialists/index.js
+++ src/modules/specialists/components/Specialists/index.js
@@ -9,9 +9,11 @@ import { connect } from 'react-redux';
 const Specialists = ({ loadingUserData, show = 3, specialists, setA11yMessage }) => {
   const [showSpecialists, setShowSpecialists] = useState(false);
   const specialistsLength = specialists?.length;
+
   if (loadingUserData || !specialistsLength) {
     return null;
   }
+
   const message = (a11y = false) => {
     return `Show${a11y ? 'ing' : ''} ${showSpecialists ? 'fewer' : `all ${specialistsLength}`} Library Specialists`;
   };
@@ -22,47 +24,45 @@ const Specialists = ({ loadingUserData, show = 3, specialists, setA11yMessage })
 
   return (
     <section className='container__rounded specialists'>
-      <div className='specialists__heading'>
-        <h2 className='specialist__heading'>Talk to a Library Specialist</h2>
+      <header>
+        <h2 className='specialists__heading'>Talk to a Library Specialist</h2>
         <Anchor href='https://www.lib.umich.edu/research-and-scholarship/help-research/find-specialist'>
           Find more specialists
           <Icon name='launch' />
         </Anchor>
-      </div>
-      <section className='specialists__content'>
-        <div className='specialists__list'>
-          {specialists.map((specialist, index) => {
-            if (showSpecialists || (show > index)) {
-              return (
-                <article className='specialist' key={`specialist-${index}`}>
-                  <img src={specialist.picture} alt='' className='specialist__picture' />
-                  <section>
-                    <h3 className='specialist__heading'>
-                      <Anchor href={specialist.url}>
-                        {specialist.name}
-                        <Icon name='launch' />
-                      </Anchor>
-                    </h3>
-                    {specialist.job_title && (<p>{specialist.job_title}</p>)}
-                    {(specialist.phone && specialist.phone !== '000-000-0000') && (<Anchor href={`tel:+1-${specialist.phone}`}>{specialist.phone}</Anchor>)}
-                    {specialist.email && (<Anchor href={`mailto:${specialist.email}`}>{specialist.email}</Anchor>)}
-                  </section>
-                </article>
-              );
-            }
-            return null;
-          })}
-        </div>
-        {specialistsLength > show && (
-          <button
-            onClick={toggleSpecialists}
-            className='btn btn--tertiary btn--small'
-            aria-expanded={showSpecialists}
-          >
-            {message()}
-          </button>
-        )}
-      </section>
+      </header>
+      <ol className='specialists__list'>
+        {specialists.map((specialist, index) => {
+          if (showSpecialists || (show > index)) {
+            return (
+              <li className='specialist' key={`specialist-${index}`}>
+                <img src={specialist.picture} alt='' className='specialist__picture' />
+                <section>
+                  <h3 className='specialists__heading'>
+                    <Anchor href={specialist.url}>
+                      {specialist.name}
+                      <Icon name='launch' />
+                    </Anchor>
+                  </h3>
+                  {specialist.job_title && (<p>{specialist.job_title}</p>)}
+                  {(specialist.phone && specialist.phone !== '000-000-0000') && (<Anchor href={`tel:+1-${specialist.phone}`}>{specialist.phone}</Anchor>)}
+                  {specialist.email && (<Anchor href={`mailto:${specialist.email}`}>{specialist.email}</Anchor>)}
+                </section>
+              </li>
+            );
+          }
+          return null;
+        })}
+      </ol>
+      {specialistsLength > show && (
+        <button
+          onClick={toggleSpecialists}
+          className='btn btn--tertiary btn--small'
+          aria-expanded={showSpecialists}
+        >
+          {message()}
+        </button>
+      )}
     </section>
   );
 };
