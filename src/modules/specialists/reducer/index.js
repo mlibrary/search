992e04ef8a35992489caf68d9bddd4392699c5d6
Erin E. Sullivan <sulliv.erin@gmail.com>
2024-03-01 13:16:16 -0500

    Creating `simpleReducer` and replacing `specialistReducer` with it. - Many reducers follow the same logic, which will be replaced by `simpleReducer`.

--- src/modules/specialists/reducer/index.js
+++
@@ -1,7 +0,0 @@
-import { ADD_SPECIALISTS } from '../actions';
-
-const specialistsReducer = (state = [], action) => {
-  return action.type === ADD_SPECIALISTS ? action.payload : state;
-};
-
-export default specialistsReducer;
--- src/store.js
+++ src/store.js
@@ -1,38 +1,42 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
-import { datastoresReducer } from './modules/datastores';
-import { searchReducer } from './modules/search';
-import { recordsReducer } from './modules/records';
-import { filtersReducer } from './modules/filters';
-import { advancedReducer } from './modules/advanced';
-import { institutionReducer } from './modules/institution';
-import { browseReducer } from './modules/browse';
-import { specialistsReducer } from './modules/specialists';
-import { listsReducer } from './modules/lists';
 import { a11yReducer } from './modules/a11y';
-import { profileReducer } from './modules/profile';
+import { advancedReducer } from './modules/advanced';
 import { affiliationReducer } from './modules/affiliation';
-
+import { browseReducer } from './modules/browse';
+import { datastoresReducer } from './modules/datastores';
+import { filtersReducer } from './modules/filters';
+import { institutionReducer } from './modules/institution';
+import { profileReducer } from './modules/profile';
+import { recordsReducer } from './modules/records';
+import { listsReducer } from './modules/lists'; // Cannot list higher than recordsReducer. `TypeError: Cannot read properties of undefined (reading 'default')`
 import history from './history';
+import { searchReducer } from './modules/search';
+import { ADD_SPECIALISTS } from './modules/specialists/actions';
+
+const simpleReducer = (initialState, actionType) => {
+  return (state = initialState, action) => {
+    return action.type === actionType ? action.payload : state;
+  };
+};
 
 const store = configureStore({
   reducer: {
-    router: connectRouter(history),
-    datastores: datastoresReducer,
-    records: recordsReducer,
-    search: searchReducer,
-    filters: filtersReducer,
-    advanced: advancedReducer,
-    institution: institutionReducer,
-    browse: browseReducer,
-    specialists: specialistsReducer,
-    lists: listsReducer,
     a11y: a11yReducer,
+    affiliation: affiliationReducer,
+    advanced: advancedReducer,
+    browse: browseReducer,
+    datastores: datastoresReducer,
+    filters: filtersReducer,
+    institution: institutionReducer,
+    lists: listsReducer,
     profile: profileReducer,
-    affiliation: affiliationReducer
+    records: recordsReducer,
+    router: connectRouter(history),
+    search: searchReducer,
+    specialists: simpleReducer([], ADD_SPECIALISTS)
   },
   middleware: (getDefaultMiddleware) => {
-    console.log(getDefaultMiddleware);
     return getDefaultMiddleware().concat(routerMiddleware(history));
   }
 });
