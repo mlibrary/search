b69b211e0f76cfd852c64028312a4b65fad95566
Erin E. Sullivan <sulliv.erin@gmail.com>
2024-03-01 14:59:31 -0500

    Simplifying reducer to not needing external variables.

--- src/modules/datastores/reducer/index.js
+++ src/modules/datastores/reducer/index.js
@@ -1,40 +1,37 @@
-import * as actions from '../actions/';
+import { ADD_DATASTORE, CHANGE_ACTIVE_DATASTORE } from '../actions/';
 
-const datastoreReducer = (state, action) => {
-  switch (action.type) {
-    case actions.ADD_DATASTORE:
-      return {
-        uid: action.payload.uid,
-        name: action.payload.name,
-        slug: action.payload.slug,
-        isMultisearch: action.payload.isMultisearch
-      };
-    default:
-      return state;
+const datastoresReducer = (
+  state = {
+    active: null,
+    datastores: []
+  },
+  action
+) => {
+  const { type, payload } = action;
+
+  if (type === ADD_DATASTORE) {
+    return {
+      ...state,
+      datastores: [
+        ...state.datastores,
+        {
+          uid: payload.uid,
+          name: payload.name,
+          slug: payload.slug,
+          isMultisearch: payload.isMultisearch
+        }
+      ]
+    };
   }
-};
 
-const initialState = {
-  active: null,
-  datastores: []
-};
-
-const datastoresReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case actions.ADD_DATASTORE:
-      return Object.assign({}, state, {
-        datastores: [
-          ...state.datastores,
-          datastoreReducer(undefined, action)
-        ]
-      });
-    case actions.CHANGE_ACTIVE_DATASTORE:
-      return Object.assign({}, state, {
-        active: action.payload
-      });
-    default:
-      return state;
+  if (type === CHANGE_ACTIVE_DATASTORE) {
+    return {
+      ...state,
+      active: payload
+    };
   }
+
+  return state;
 };
 
 export default datastoresReducer;
