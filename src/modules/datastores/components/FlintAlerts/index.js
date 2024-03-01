4499776c50f5d42001a0d383ddc577a73f696cdd
Erin E. Sullivan <sulliv.erin@gmail.com>
2024-03-01 14:40:51 -0500

    Improving `FlintAlerts` logic. Adding `handleDismissClick` function to handle each datastore, not all datastores.

--- src/modules/datastores/components/FlintAlerts/index.js
+++ src/modules/datastores/components/FlintAlerts/index.js
@@ -1,34 +1,35 @@
 import React, { useState } from 'react';
-import { Alert, Anchor, Button } from '../../../reusable';
+import { Alert, Anchor } from '../../../reusable';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-function FlintAlerts (props) {
-  const [closed, setClosed] = useState(false);
-  const datastores = ['primo', 'databases', 'onlinejournals', 'website'];
+function FlintAlerts ({ datastore, profile }) {
+  const [dismiss, setDismiss] = useState([]);
+  const handleDismissClick = () => {
+    setDismiss((previousDismiss) => {
+      return [...previousDismiss, datastore];
+    });
+  };
+  const messages = {
+    primo: (<>U-M Flint users: You may not be able to access U-M Ann Arbor resources. For the best results use <Anchor href='https://umich.primo.exlibrisgroup.com/discovery/search?vid=01UMICH_INST:FLINT'>Thompson Library’s Search All</Anchor> to search for articles.</>),
+    databases: (<>We noticed you are affiliated with U-M Flint. For the best results use the <Anchor href='https://libguides.umflint.edu/az.php?a=all'>Thompson Library’s database listing</Anchor>.</>),
+    onlinejournals: (<>We noticed you are affiliated with U-M Flint. For the best results use the <Anchor href='https://umich.primo.exlibrisgroup.com/discovery/jsearch?vid=01UMICH_INST:FLINT'>Thompson Library’s Search All</Anchor> to search for articles.</>),
+    website: (<>We noticed you are affiliated with U-M Flint. For the best results use the <Anchor href='https://libguides.umflint.edu/library'>Thompson Library website</Anchor>.</>)
+  };
 
-  if (!datastores.includes(props.datastore) || !props.profile.institutions?.includes('Flint') || closed) {
+  if (!Object.keys(messages).includes(datastore) || !profile.institutions?.includes('Flint') || dismiss.includes(datastore)) {
     return null;
   }
 
   return (
     <Alert type='warning'>
-      <span>
-        {props.datastore === datastores[0] && <>U-M Flint users: You may not be able to access U-M Ann Arbor resources. For the best results use <Anchor href='https://umich.primo.exlibrisgroup.com/discovery/search?vid=01UMICH_INST:FLINT'>Thompson Library’s Search All</Anchor> to search for articles.</>}
-        {props.datastore === datastores[1] && <>We noticed you are affiliated with U-M Flint. For the best results use the <Anchor href='https://libguides.umflint.edu/az.php?a=all'>Thompson Library’s database listing</Anchor>.</>}
-        {props.datastore === datastores[2] && <>We noticed you are affiliated with U-M Flint. For the best results use the <Anchor href='https://umich.primo.exlibrisgroup.com/discovery/jsearch?vid=01UMICH_INST:FLINT'>Thompson Library’s Search All</Anchor> to search for articles.</>}
-        {props.datastore === datastores[3] && <>We noticed you are affiliated with U-M Flint. For the best results use the <Anchor href='https://libguides.umflint.edu/library'>Thompson Library website</Anchor>.</>}
-      </span>
-      <Button
-        kind='secondary'
-        small
-        onClick={(event) => {
-          event.preventDefault();
-          setClosed(true);
-        }}
+      <span>{messages[datastore]}</span>
+      <button
+        className='btn btn--small btn--secondary'
+        onClick={handleDismissClick}
       >
         Dismiss
-      </Button>
+      </button>
     </Alert>
   );
 }
@@ -38,10 +39,8 @@ FlintAlerts.propTypes = {
   datastore: PropTypes.string
 };
 
-function mapStateToProps (state) {
+export default connect((state) => {
   return {
     profile: state.profile
   };
-}
-
-export default connect(mapStateToProps)(FlintAlerts);
+})(FlintAlerts);
