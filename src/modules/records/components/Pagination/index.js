f04c68178c466d0a4f12ba11f71f291972fa1e3a
Erin E. Sullivan <sulliv.erin@gmail.com>
2024-03-05 10:44:52 -0500

    Moving `PaginationPresenter` directly into `Pagination`.

--- src/modules/records/components/Pagination/index.js
+++ src/modules/records/components/Pagination/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { stringifySearchQueryForURL } from '../../../pride';
-import PaginationPresenter from './presenter';
+import './styles.css';
+import { Anchor } from '../../../reusable';
 
 const Pagination = () => {
   const history = useHistory();
@@ -28,11 +29,21 @@ const Pagination = () => {
     };
   });
 
-  const createSearchQuery = (page) => {
+  const [currentPage, setCurrentPage] = useState(page);
+
+  useEffect(() => {
+    setCurrentPage(page);
+  }, [page]);
+
+  if (!records || records.length === 0) {
+    return null;
+  }
+
+  const createSearchQuery = (newPage) => {
     const queryString = stringifySearchQueryForURL({
       filter: filters,
       library: activeDatastoreUid === 'mirlyn' ? institution.active : undefined,
-      page,
+      page: newPage,
       query: search.query,
       sort
     });
@@ -40,31 +51,57 @@ const Pagination = () => {
     return `${history.location.pathname}?${queryString}`;
   };
 
-  const toPreviousPage = () => {
-    return page === 1 ? undefined : createSearchQuery(page - 1);
-  };
-
-  const toNextPage = () => {
-    return total === 0 || page === total ? undefined : createSearchQuery(page + 1);
-  };
-
+  const toPreviousPage = page === 1 ? undefined : createSearchQuery(page - 1);
+  const toNextPage = total === 0 || page === total ? undefined : createSearchQuery(page + 1);
   const watchPageChange = (newPage) => {
-    history.push(createSearchQuery(newPage));
+    return history.push(createSearchQuery(newPage));
   };
 
-  if (!records || records.length === 0) {
-    return null;
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const pageNum = parseInt(currentPage, 10);
+    if (Number.isInteger(pageNum) && pageNum > 0 && pageNum <= total) {
+      watchPageChange(pageNum);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setCurrentPage(e.target.value);
+  };
+
+  const totalConverted = total?.toLocaleString();
 
   return (
-    <PaginationPresenter
-      ariaLabel='Pagination'
-      page={page}
-      total={total}
-      watchPageChange={watchPageChange}
-      toNextPage={toNextPage()}
-      toPreviousPage={toPreviousPage()}
-    />
+    <nav className='pagination-container' aria-label='Pagination'>
+      <div className='pagination-previous-container'>
+        {toPreviousPage && (
+          <Anchor to={toPreviousPage}>
+            Previous page
+          </Anchor>
+        )}
+      </div>
+      <form className='pagination-input-container' onSubmit={handleSubmit}>
+        <span>Page</span>
+        <input
+          className='pagination-input'
+          value={currentPage}
+          type='number'
+          aria-label={`Page ${currentPage} of ${totalConverted} pages`}
+          onBlur={() => {
+            return setCurrentPage(page);
+          }}
+          onChange={handleInputChange}
+        />
+        <span>of&nbsp;{totalConverted}</span>
+      </form>
+      <div className='pagination-next-container'>
+        {toNextPage && (
+          <Anchor to={toNextPage}>
+            Next page
+          </Anchor>
+        )}
+      </div>
+    </nav>
   );
 };
 
