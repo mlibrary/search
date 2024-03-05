d59979d43ed5760c45a0ce5237933926639cf5b8
Erin E. Sullivan <sulliv.erin@gmail.com>
2024-03-05 14:49:28 -0500

    Converting `InstitutionWrapper` into a functional component.

--- src/modules/institution/components/InstitutionWrapper/index.js
+++ src/modules/institution/components/InstitutionWrapper/index.js
@@ -1,73 +1,48 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import { useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { stringifySearchQueryForURL } from '../../../pride';
-import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-/*
-  Adds the Institution (aka Library or Library Scope) filter
-  to the URL if it is not already active.
-*/
-class InstitutionWrapper extends React.Component {
-  pushLibraryScopeToURL = () => {
-    const { datastore, institution } = this.props;
+const InstitutionWrapper = ({ children }) => {
+  const history = useHistory();
+  const params = useParams();
+  const {
+    activeFilters,
+    datastore,
+    institution,
+    search
+  } = useSelector((state) => {
+    return {
+      activeFilters: state.filters.active,
+      datastore: state.datastores.active,
+      institution: state.institution,
+      search: state.search
+    };
+  });
 
+  useEffect(() => {
     if (datastore === 'mirlyn' && !institution.active) {
-      const {
-        match,
-        history,
-        search,
-        activeFilters
-      } = this.props;
-
-      const library = datastore.uid === 'mirlyn' ? institution.active : institution.defaultInstitution;
-      const page = search.page[datastore.uid] === 1 ? undefined : search.page[datastore.uid];
       const queryString = stringifySearchQueryForURL({
         query: search.query,
         filter: activeFilters[datastore.uid],
-        page,
+        page: search.page[datastore.uid] === 1 ? undefined : search.page[datastore.uid],
         sort: search.sort[datastore.uid],
-        library
+        library: datastore.uid === 'mirlyn' ? institution.active : institution.defaultInstitution
       });
 
-      const url = `/${match.params.datastoreSlug}?${queryString}`;
-      history.push(url);
+      history.push(`/${params.datastoreSlug}?${queryString}`);
     }
-  };
+  }, [history, params.datastoreSlug, datastore, institution, search, activeFilters]);
 
-  componentDidMount () {
-    this.pushLibraryScopeToURL();
-  }
-
-  componentDidUpdate () {
-    this.pushLibraryScopeToURL();
-  }
-
-  render () {
-    return this.props.children;
-  }
-}
+  return children;
+};
 
 InstitutionWrapper.propTypes = {
-  datastore: PropTypes.string,
-  institution: PropTypes.object,
-  match: PropTypes.object,
-  history: PropTypes.object,
-  search: PropTypes.object,
-  activeFilters: PropTypes.object,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node
   ])
 };
 
-function mapStateToProps (state) {
-  return {
-    datastore: state.datastores.active,
-    institution: state.institution,
-    search: state.search,
-    activeFilters: state.filters.active
-  };
-}
-
-export default withRouter(connect(mapStateToProps)(InstitutionWrapper));
+export default InstitutionWrapper;
