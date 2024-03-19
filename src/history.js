bfbc229fbb0e80f4dc12b8539db39ba87585fde7
Erin E. Sullivan <sulliv.erin@gmail.com>
2024-03-19 15:47:08 -0400

    Uninstalling `connected-react-router` and `history` as they are no longer being used.

--- src/history.js
+++
@@ -1,6 +0,0 @@
-const createHistory = require('history').createBrowserHistory;
-
-// Create a history of your choosing (we're using a browser history in this case)
-const history = createHistory();
-
-export default history;
