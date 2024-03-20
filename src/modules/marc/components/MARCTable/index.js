b3f52ddb875a0404976570807d26d29b03f10e96
Erin E. Sullivan <sulliv.erin@gmail.com>
2024-03-20 14:55:13 -0400

    Converting `MARCTable` into a functional component.

--- src/modules/marc/components/MARCTable/index.js
+++ src/modules/marc/components/MARCTable/index.js
@@ -1,116 +1,72 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Leader = ({ marc }) => {
-  if (marc.leader) {
-    return (
-      <tr>
-        <td className='marc__field-name' colSpan='3'><abbr title='LEADER'>LDR</abbr></td>
-        <td>{marc.leader}</td>
-      </tr>
-    );
-  }
-
-  return null;
-};
-
-Leader.propTypes = {
-  marc: PropTypes.object
-};
-
-const FieldName = ({ field }) => {
-  const name = Object.keys(field)[0];
-
-  return (
-    <td className='marc__field-name'>
-      {name}
-    </td>
-  );
-};
-
-FieldName.propTypes = {
-  field: PropTypes.object
-};
-
-const FieldIndicator = ({ field, ind }) => {
-  const name = Object.keys(field)[0];
-  const value = field[name];
-  const indicator = value[ind];
-
-  return (
-    <td>{indicator}</td>
-  );
-};
-
-FieldIndicator.propTypes = {
-  field: PropTypes.object,
-  ind: PropTypes.string
+const name = (field) => {
+  return Object.keys(field)[0];
 };
 
 const FieldValue = ({ field }) => {
-  const name = Object.keys(field)[0];
-  const value = field[name];
+  const value = field[name(field)];
 
   if (typeof value === 'string') {
-    return (
-      <td>
-        {field[name]}
-      </td>
-    );
-  }
+    return value;
+  };
 
-  if (typeof value === 'object') {
-    if (value.subfields) {
-      return (
-        <td>
-          {value.subfields.map((subfield, index) => {
-            const subfieldName = Object.keys(subfield)[0];
-
-            return (
-              <span className='marc__subfield' key={index}><span className='strong'>|{subfieldName}</span> {subfield[subfieldName]}</span>
-            );
-          })}
-        </td>
-      );
-    }
-  }
-
+  const subfields = value.subfields;
   return (
-    <td>
-      temp
-    </td>
+    <>
+      {subfields?.map((subfield, index) => {
+        const subfieldName = name(subfield);
+        return (
+          <span className='marc__subfield' key={index}>
+            <span className='strong'>|{subfieldName}</span> {subfield[subfieldName]}
+          </span>
+        );
+      })}
+    </>
   );
 };
 
 FieldValue.propTypes = {
-  field: PropTypes.object
+  field: PropTypes.object.isRequired
 };
 
-class MARCTable extends React.Component {
-  render () {
-    const { marc } = this.props;
-
-    return (
-      <div className='marc__container'>
-        <h2 className='marc__heading'>MARC Data</h2>
-        <table className='table marc__table'>
-          <tbody>
-            <Leader marc={marc} />
-            {marc.fields.map((field, index) => {
-              return (
-                <tr key={index}>
-                  <FieldName field={field} />
-                  <FieldIndicator field={field} ind='ind1' />
-                  <FieldIndicator field={field} ind='ind2' />
+function MARCTable ({ marc }) {
+  return (
+    <div className='marc__container'>
+      <h2 className='marc__heading'>MARC Data</h2>
+      <table className='table marc__table'>
+        <tbody>
+          {marc.leader && (
+            <tr>
+              <td className='marc__field-name' colSpan='3'><abbr title='LEADER'>LDR</abbr></td>
+              <td>{marc.leader}</td>
+            </tr>
+          )}
+          {marc.fields.map((field, index) => {
+            const fieldName = name(field);
+            return (
+              <tr key={index}>
+                <td className='marc__field-name'>
+                  {fieldName}
+                </td>
+                {['ind1', 'ind2'].map((indicator) => {
+                  return (
+                    <td key={`${indicator}-${index}`}>
+                      {field[fieldName][indicator]}
+                    </td>
+                  );
+                })}
+                <td>
                   <FieldValue field={field} />
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 MARCTable.propTypes = {
