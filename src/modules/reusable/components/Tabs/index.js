b6897f66c3e7f8c40ad6a964caf1da0f354efb98
Erin E. Sullivan <sulliv.erin@gmail.com>
2024-03-25 15:41:58 -0400

    Adding styles.

--- src/modules/reusable/components/Tabs/index.js
+++ src/modules/reusable/components/Tabs/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import './styles.css';
 import PropTypes from 'prop-types';
 
 function Tabs ({ children, defaultActiveIndex = 0, ...rest }) {
