1562f591774bc58e68a2b58f7e9db53bfaefcf95
Erin E. Sullivan <sulliv.erin@gmail.com>
2024-03-25 09:48:27 -0400

    Creating a new deployment for Ken using production Spectrum.

--- src/modules/resource-acccess/components/holders.js
+++ src/modules/resource-acccess/components/holders.js
@@ -23,14 +23,11 @@ export default function Holders ({
       (Object.keys(mirlyn).includes('search_only') && mirlyn.search_only.includes('true'))
     )
   ) {
-    // UNCOMMENT THE BLOCK BELOW WHEN READY TO LAUNCH
-    /*
     record.resourceAccess.forEach((resource) => {
       resource.rows = resource.rows.filter((row) => {
         return row[0].text !== 'Search only (no full text)';
       });
     });
-    */
   }
   return (
     <>
