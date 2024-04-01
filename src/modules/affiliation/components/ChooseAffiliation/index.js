fa120b2cda6787a2c02639535a99b1b1d9f5add8
Erin E. Sullivan <sulliv.erin@gmail.com>
2024-04-01 08:29:00 -0400

    Show `Dialog` only if not on an old version of Safari.

--- src/modules/affiliation/components/ChooseAffiliation/index.js
+++ src/modules/affiliation/components/ChooseAffiliation/index.js
@@ -77,26 +77,28 @@ export default function ChooseAffiliation () {
           );
         })}
       </button>
-      <Dialog open={isDialogOpen} isOpen={isDialogOpen} onClose={handleClose}>
-        <h2 className='margin-top__none'>
-          Choose campus affiliation
-        </h2>
-        <p>
-          Selecting an affiliation helps us connect you to available online materials licensed for your campus.
-        </p>
-        <div className='dialog-buttons'>
-          <button className='btn btn--primary' onClick={handleClose}>
-            Continue as {affiliationOptions[affiliation]}
-          </button>
-          or
-          <button className='btn btn--secondary' onClick={changeAffiliation} role='link'>
-            Change to {affiliationOptions[alternativeAffiliation]}
-          </button>
-        </div>
-        <p className='font-small margin-bottom__none'>
-          You can still use Library Search if you're not affiliated with either campus.
-        </p>
-      </Dialog>
+      {!oldSafari() && (
+        <Dialog open={isDialogOpen} isOpen={isDialogOpen} onClose={handleClose}>
+          <h2 className='margin-top__none'>
+            Choose campus affiliation
+          </h2>
+          <p>
+            Selecting an affiliation helps us connect you to available online materials licensed for your campus.
+          </p>
+          <div className='dialog-buttons'>
+            <button className='btn btn--primary' onClick={handleClose}>
+              Continue as {affiliationOptions[affiliation]}
+            </button>
+            or
+            <button className='btn btn--secondary' onClick={changeAffiliation} role='link'>
+              Change to {affiliationOptions[alternativeAffiliation]}
+            </button>
+          </div>
+          <p className='font-small margin-bottom__none'>
+            You can still use Library Search if you're not affiliated with either campus.
+          </p>
+        </Dialog>
+      )}
     </div>
   );
 }
