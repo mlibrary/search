b76cef6f01142c740207a47c9e59cd34f1bf19f9
Erin E. Sullivan <sulliv.erin@gmail.com>
2024-08-12 09:42:35 -0400

    Moving `ShelfBrowseLoading` into `ShelfBrowse` and adding states to control button actions and attributes from child components.

--- src/modules/browse/components/ShelfBrowse/index.js
+++ src/modules/browse/components/ShelfBrowse/index.js
@@ -1,10 +1,28 @@
+import './styles.css';
+import { Icon, useWindowWidth } from '../../../reusable';
 import React, { useEffect, useState } from 'react';
+import BrowseLink from '../BrowseLink';
 import ShelfBrowseCarousel from '../ShelfBrowseCarousel';
-import ShelfBrowseLoading from '../ShelfBrowseLoading';
 import { useSelector } from 'react-redux';
 
 const ShelfBrowse = () => {
   const [shelfData, setShelfData] = useState();
+  const [disableButton, setDisableButton] = useState({
+    currentRecord: true,
+    nextRecords: true,
+    previousRecords: true
+  });
+  const [buttonAction, setButtonAction] = useState({
+    currentRecord () {
+      //
+    },
+    nextRecords () {
+      //
+    },
+    previousRecords () {
+      //
+    }
+  });
   const { metadata, uid } = useSelector((state) => {
     return state.records.record;
   });
@@ -19,6 +37,15 @@ const ShelfBrowse = () => {
 
   if (!callNumberBrowse) {
     return null;
+  }
+
+  const windowWidth = useWindowWidth();
+  let itemsPerPage = 5;
+  if (windowWidth < 820) {
+    itemsPerPage = 3;
+  }
+  if (windowWidth < 640) {
+    itemsPerPage = 1;
   }
 
   const callNumber = callNumberBrowse.browse.value.trim();
@@ -45,11 +72,83 @@ const ShelfBrowse = () => {
     return null;
   }
 
-  if (shelfData === 'loading') {
-    return <ShelfBrowseLoading {...{ callNumber }} />;
-  }
+  const loadingItems = new Array(itemsPerPage).fill(null);
+  const buttonLabel = `${itemsPerPage} record${itemsPerPage === 1 ? '' : 's'}`;
 
-  return <ShelfBrowseCarousel {...{ callNumber, items: shelfData, uid }} />;
+  return (
+    <section className='shelf-browse container__rounded'>
+      <header className='flex__responsive'>
+        <h2 className='margin-y__none heading-medium'>Shelf browse</h2>
+        <BrowseLink value={callNumber}>
+          <Icon icon='list' size='24' className='margin-right__xs' />Browse in call number list
+        </BrowseLink>
+      </header>
+      <div className='shelf-browse-carousel' aria-label='Shelf browse carousel'>
+        <button
+          aria-label={`Previous ${buttonLabel}`}
+          title={`Previous ${buttonLabel}`}
+          disabled={disableButton.previousRecords}
+          onClick={buttonAction.previousRecords}
+          className='btn no-background'
+        >
+          <Icon icon='chevron_left' size='24' />
+        </button>
+        <ul
+          className='list__unstyled shelf-browse-items'
+          style={{
+            gridTemplateColumns: `repeat(${itemsPerPage}, 1fr)`
+          }}
+        >
+          {shelfData === 'loading'
+            ? loadingItems.map((element, index) => {
+              return (
+                <li key={index} className='shelf-browse-item'>
+                  <div className='container__rounded padding-x__s padding-bottom__xs padding-top__s'>
+                    <div className='placeholder-item-title'>
+                      <div className='placeholder placeholder-title margin-bottom__none' />
+                      <div className='placeholder placeholder-title margin-bottom__none margin-top__2xs' />
+                      <div className='placeholder placeholder-title margin-bottom__none margin-top__2xs' />
+                    </div>
+                    <div className='placeholder-item-line'>
+                      <div className='placeholder placeholder-line margin-bottom__none margin-top__s' />
+                      <div className='placeholder placeholder-line margin-bottom__none margin-top__s' />
+                      <div className='placeholder placeholder-line placeholder-line-alt margin-top__s' />
+                    </div>
+                  </div>
+                </li>
+              );
+            })
+            : (
+                <ShelfBrowseCarousel {...{
+                  callNumber,
+                  items: shelfData,
+                  itemsPerPage,
+                  setButtonAction,
+                  setDisableButton,
+                  uid
+                }}
+                />
+              )}
+        </ul>
+        <button
+          aria-label={`Next ${buttonLabel}`}
+          title={`Next ${buttonLabel}`}
+          disabled={disableButton.nextRecords}
+          onClick={buttonAction.nextRecords}
+          className='btn no-background'
+        >
+          <Icon icon='chevron_right' size='24' />
+        </button>
+      </div>
+      <button
+        className='btn btn--secondary btn--small shelf-browse-carousel-return'
+        onClick={buttonAction.currentRecord}
+        disabled={disableButton.currentRecord}
+      >
+        Return to current record
+      </button>
+    </section>
+  );
 };
 
 export default ShelfBrowse;
--- src/modules/browse/components/ShelfBrowseCarousel/index.js
+++ src/modules/browse/components/ShelfBrowseCarousel/index.js
@@ -1,24 +1,15 @@
 import './styles.css';
-import { Anchor, Icon, useWindowWidth } from '../../../reusable';
+import { Anchor, Icon } from '../../../reusable';
 import React, { useEffect, useState } from 'react';
-import BrowseLink from '../BrowseLink';
 import PropTypes from 'prop-types';
 
-const ShelfBrowseCarousel = ({ callNumber, items, uid }) => {
+const ShelfBrowseCarousel = ({ callNumber, items, itemsPerPage, setButtonAction, setDisableButton, uid }) => {
   const currentItem = (item) => {
     return item.call_number === callNumber && item.url.endsWith(uid);
   };
   const callNumberIndex = items.findIndex((item) => {
     return currentItem(item);
   });
-  const windowWidth = useWindowWidth();
-  let itemsPerPage = 5;
-  if (windowWidth < 820) {
-    itemsPerPage = 3;
-  }
-  if (windowWidth < 640) {
-    itemsPerPage = 1;
-  }
   const middlePage = Math.floor(callNumberIndex / itemsPerPage);
   const [currentPage, setCurrentPage] = useState(middlePage);
   const [animationClass, setAnimationClass] = useState('');
@@ -68,91 +59,11 @@ const ShelfBrowseCarousel = ({ callNumber, items, uid }) => {
     }, animationDuration * 2);
   };
 
-  const startIndex = currentPage * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  const currentItems = items.slice(startIndex, endIndex);
-  const firstPage = currentPage === 0;
-  const lastPage = currentPage === maxPages - 1;
-  const buttonLabel = `${itemsPerPage} record${itemsPerPage === 1 ? '' : 's'}`;
-
-  return (
-    <section className='shelf-browse container__rounded'>
-      <header className='flex__responsive'>
-        <h2 className='margin-y__none heading-medium'>Shelf browse</h2>
-        <BrowseLink value={callNumber}>
-          <Icon icon='list' size='24' className='margin-right__xs' />Browse in call number list
-        </BrowseLink>
-      </header>
-      <div className='shelf-browse-carousel' aria-label='Shelf browse carousel'>
-        <button
-          aria-label={`Previous ${buttonLabel}`}
-          title={`Previous ${buttonLabel}`}
-          disabled={firstPage}
-          onClick={() => {
-            return moveCarousel(-1);
-          }}
-          className='btn no-background'
-        >
-          <Icon icon='chevron_left' size='24' />
-        </button>
-        <ul
-          className='list__unstyled shelf-browse-items'
-          style={{
-            gridTemplateColumns: `repeat(${itemsPerPage}, 1fr)`
-          }}
-        >
-          {currentItems.map((item, index) => {
-            const isCurrentItem = currentItem(item);
-            const firstOrLastItem = !isCurrentItem && ((firstPage && index === 0) || (lastPage && currentItems.length - 1 === index));
-            const fields = firstOrLastItem ? ['call_number'] : Object.keys(item).slice(0, -1);
-            const basePath = 'https://search.lib.umich.edu';
-            const anchorAttributes = firstOrLastItem
-              ? { href: `${basePath}/catalog/browse/callnumber?query=${item.call_number}` }
-              : { to: item.url.replace(basePath, '') + window.location.search };
-
-            return (
-              <li key={index} className={`shelf-browse-item ${(isCurrentItem || firstOrLastItem) ? 'shelf-browse-item-current' : ''} ${animationClass}`}>
-                <Anchor
-                  {...anchorAttributes}
-                  className={`focus underline__none container__rounded padding-x__s padding-bottom__xs padding-top__${isCurrentItem ? 'xs' : 's'}`}
-                >
-                  <dl className='flex'>
-                    {isCurrentItem && <p className='margin__none this-item'>Current Record</p>}
-                    {firstOrLastItem && (
-                      <>
-                        <Icon icon='list' size='24' className='item-term-title' />
-                        <span className='item-term-title'>Continue browsing in call number list</span>
-                      </>
-                    )}
-                    {fields.map((key) => {
-                      return item[key] && (
-                        <React.Fragment key={key}>
-                          <dt className='visually-hidden'>{key.replaceAll('_', ' ')}</dt>
-                          <dd className={`item-term-${key}`}>{item[key]}</dd>
-                        </React.Fragment>
-                      );
-                    })}
-                  </dl>
-                </Anchor>
-              </li>
-            );
-          })}
-        </ul>
-        <button
-          aria-label={`Next ${buttonLabel}`}
-          title={`Next ${buttonLabel}`}
-          disabled={lastPage}
-          onClick={() => {
-            return moveCarousel(1);
-          }}
-          className='btn no-background'
-        >
-          <Icon icon='chevron_right' size='24' />
-        </button>
-      </div>
-      <button
-        className='btn btn--secondary btn--small shelf-browse-carousel-return'
-        onClick={() => {
+  useEffect(() => {
+    setButtonAction((prevState) => {
+      return {
+        ...prevState,
+        currentRecord: () => {
           setAnimationClass('animation-out');
           setTimeout(() => {
             setCurrentPage(middlePage);
@@ -161,18 +72,82 @@ const ShelfBrowseCarousel = ({ callNumber, items, uid }) => {
           setTimeout(() => {
             setAnimationClass('');
           }, animationDuration * 2);
-        }}
-        disabled={currentPage === middlePage}
-      >
-        Return to current record
-      </button>
-    </section>
+        },
+        nextRecords: () => {
+          return moveCarousel(1);
+        },
+        previousRecords: () => {
+          return moveCarousel(-1);
+        }
+      };
+    });
+  }, [setButtonAction]);
+
+  const startIndex = currentPage * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentItems = items.slice(startIndex, endIndex);
+  const firstPage = currentPage === 0;
+  const lastPage = currentPage === maxPages - 1;
+
+  useEffect(() => {
+    setDisableButton((prevState) => {
+      return {
+        ...prevState,
+        currentRecord: currentPage === middlePage,
+        nextRecords: firstPage,
+        previousRecords: lastPage
+      };
+    });
+  }, [currentPage, firstPage, lastPage, setDisableButton]);
+
+  return (
+    <>
+      {currentItems.map((item, index) => {
+        const isCurrentItem = currentItem(item);
+        const firstOrLastItem = !isCurrentItem && ((firstPage && index === 0) || (lastPage && currentItems.length - 1 === index));
+        const fields = firstOrLastItem ? ['call_number'] : Object.keys(item).slice(0, -1);
+        const basePath = 'https://search.lib.umich.edu';
+        const anchorAttributes = firstOrLastItem
+          ? { href: `${basePath}/catalog/browse/callnumber?query=${item.call_number}` }
+          : { to: item.url.replace(basePath, '') + window.location.search };
+
+        return (
+          <li key={index} className={`shelf-browse-item ${(isCurrentItem || firstOrLastItem) ? 'shelf-browse-item-current' : ''} ${animationClass}`}>
+            <Anchor
+              {...anchorAttributes}
+              className={`focus underline__none container__rounded padding-x__s padding-bottom__xs padding-top__${isCurrentItem ? 'xs' : 's'}`}
+            >
+              <dl className='flex'>
+                {isCurrentItem && <p className='margin__none this-item'>Current Record</p>}
+                {firstOrLastItem && (
+                  <>
+                    <Icon icon='list' size='24' className='item-term-title' />
+                    <span className='item-term-title'>Continue browsing in call number list</span>
+                  </>
+                )}
+                {fields.map((key) => {
+                  return item[key] && (
+                    <React.Fragment key={key}>
+                      <dt className='visually-hidden'>{key.replaceAll('_', ' ')}</dt>
+                      <dd className={`item-term-${key}`}>{item[key]}</dd>
+                    </React.Fragment>
+                  );
+                })}
+              </dl>
+            </Anchor>
+          </li>
+        );
+      })}
+    </>
   );
 };
 
 ShelfBrowseCarousel.propTypes = {
   callNumber: PropTypes.string,
   items: PropTypes.array,
+  itemsPerPage: PropTypes.number,
+  setButtonAction: PropTypes.func,
+  setDisableButton: PropTypes.func,
   uid: PropTypes.string
 };
 
