b00b819b7bcbf7448591039514451133b36c9a8b
Erin E. Sullivan <sulliv.erin@gmail.com>
2024-09-06 11:42:04 -0400

    Creating `ILLRequestMessage` and applying to zero result messages.

---
+++ src/modules/records/components/ILLRequestMessage/index.js
@@ -0,0 +1,11 @@
+import { Anchor } from '../../../reusable';
+
+const ILLRequestMessage = () => {
+  return (
+    <>
+      Place an <Anchor href='https://ill.lib.umich.edu/' utmSource='library-search-no-items'>Interlibrary Loan request</Anchor> if you need something we don't have. We'll get it from another institution.
+    </>
+  );
+};
+
+export default ILLRequestMessage;
