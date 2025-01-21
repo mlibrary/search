28da6cfaa1013377fe620539f0542b56cddbcd0c
Erin E. Sullivan <sulliv.erin@gmail.com>
2025-01-21 13:38:04 -0500

    Removing the need for `ActionStatusMessage` component.

--- src/modules/lists/components/ShareAction/index.js
+++ src/modules/lists/components/ShareAction/index.js
@@ -1,21 +1,45 @@
+import { Alert, Anchor } from '../../../reusable';
 import React, { useEffect, useState } from 'react';
-import ActionStatusMessage from '../ActionStatusMessage';
 import { AuthenticationRequired } from '../../../profile';
 import { useSelector } from 'react-redux';
 
 const ShareAction = ({ action, datastoreUid, prejudice }) => {
   const [inputValue, setInputValue] = useState('');
   const [status, setStatus] = useState();
+  const [alertType, setAlertType] = useState();
+  const [alertMessage, setAlertMessage] = useState(<>We&apos;re sorry. Something went wrong. Please use <Anchor href='https://www.lib.umich.edu/ask-librarian'>Ask a Librarian</Anchor> for help.</>);
   const { email, status: profileStatus, text } = useSelector((state) => {
     return state.profile || {};
   });
 
-  const type = action.uid;
+  const { name, uid: type } = action;
   const isEmail = type === 'email';
 
   useEffect(() => {
     setInputValue(isEmail ? email || '' : text || '');
   }, [isEmail, email, text]);
+
+  useEffect(() => {
+    if (status) {
+      const statusCode = status.status_code;
+      if (statusCode?.startsWith('action.response.')) {
+        if (statusCode.endsWith('success')) {
+          setAlertType('success');
+          setAlertMessage(<>{name} successfully sent.</>);
+        } else if (statusCode.includes('invalid.')) {
+          setAlertType('error');
+          if (statusCode.endsWith('email')) {
+            setAlertMessage(<>Please enter a valid email address (e.g. uniqname@umich.edu)</>);
+          } else if (statusCode.endsWith('number')) {
+            setAlertMessage(<>Please enter a valid 10-digit phone number (e.g. 000-123-5555)</>);
+          }
+        }
+      } else {
+        setAlertType('warning');
+        setAlertMessage(<>We&apos;re sorry. Something went wrong. Please use <Anchor href='https://www.lib.umich.edu/ask-librarian'>Ask a Librarian</Anchor> for help.</>);
+      }
+    }
+  }, [status, name]);
 
   const handleSubmitCallback = (data) => {
     setStatus(data);
@@ -24,7 +48,7 @@ const ShareAction = ({ action, datastoreUid, prejudice }) => {
   return (
     <AuthenticationRequired {...{ status: profileStatus }}>
       <section className='lists-action'>
-        <ActionStatusMessage {...{ action, status }} />
+        {status && <Alert type={alertType}>{alertMessage}</Alert>}
         {(!status || status.status_code !== 'action.response.success') && (
           <form
             className='lists-action-form'
--- src/modules/lists/index.js
+++ src/modules/lists/index.js
@@ -1,5 +1,4 @@
 import ActionsList from './components/ActionsList';
-import ActionStatusMessage from './components/ActionStatusMessage';
 import AddToListButton from './components/AddToListButton';
 import GoToList from './components/GoToList';
 import isInList from './isInList.js';
@@ -8,7 +7,6 @@ import prejudice from './prejudice.js';
 
 export {
   ActionsList,
-  ActionStatusMessage,
   AddToListButton,
   GoToList,
   isInList,
