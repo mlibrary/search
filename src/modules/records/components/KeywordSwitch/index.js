84d130e96100e4940d5f37fcc1c18148abff2c7e
Erin E. Sullivan <sulliv.erin@gmail.com>
2025-02-03 14:34:57 -0500

    Replacing Holders icons.

--- src/modules/records/components/KeywordSwitch/index.js
+++ src/modules/records/components/KeywordSwitch/index.js
@@ -23,10 +23,7 @@ const KeywordSwitch = ({ datastore, query }) => {
 
   return (
     <div className={`flex padding__m padding-left__s ${briefView ? 'record-preview' : 'container__rounded record'} keyword-switch`}>
-      <Icon
-        d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z'
-        size={24}
-      />
+      <Icon icon='error' size={24} />
       <div>
         <p className='no-margin'>
           {!briefView && isContainsSearch ? 'Seeing less precise results than you expected?' : 'Not seeing the results you expected?'}
