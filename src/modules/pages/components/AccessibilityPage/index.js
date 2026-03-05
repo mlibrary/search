6d069de706cc0ac91983af0a0c1a7b6377e7e212
Erin Sullivan <sulliv.erin@gmail.com>
2026-03-05 11:02:52 -0500

    Updating content according to https://docs.google.com/document/d/1DbwIxLbp9rjy4u3RUXRQIg_Em8HX9HaWh8ixg7EL2PQ/edit?usp=sharing

--- src/modules/pages/components/AccessibilityPage/index.js
+++ src/modules/pages/components/AccessibilityPage/index.js
@@ -10,14 +10,14 @@ const AccessibilityPage = () => {
   return (
     <div className='container container-narrow'>
       <main className='container__rounded page'>
-        <H1>Library Search Accessibility Statement</H1>
+        <H1>Library Search Accessibility</H1>
         <p>The University of Michigan Library is dedicated to creating inclusive services and products for all users. See more about our approach to <Anchor href='https://www.lib.umich.edu/about-us/about-library/accessibility/digital-product-accessibility'>digital product accessibility</Anchor>, as well as our overall <Anchor href='https://lib.umich.edu/about-us/about-library/accessibility'>accessibility services and practices</Anchor> at the U-M Library.</p>
+        <h2>Contact us</h2>
+        <p>We welcome your feedback and will do our best to resolve any issues promptly.</p>
+        <p>If you encounter any accessibility issues while using this product or have questions, please contact us <Anchor href='https://umich.qualtrics.com/jfe/form/SV_bCwYIKueEXs8wBf'>via our feedback form</Anchor>.</p>
         <h2>Standards and compliance</h2>
         <p>Our baseline accessibility target is to meet <Anchor href='https://www.w3.org/TR/WCAG21/'>WCAG 2.1 AA standards</Anchor>. The last accessibility evaluation for Library Search was in March 2023.</p>
         <p>The next scheduled accessibility evaluation for this product is in 2026.</p>
-        <h2>Feedback and support</h2>
-        <p>We welcome your feedback and will do our best to resolve any issues promptly.</p>
-        <p>If you encounter any accessibility issues while using this product or have questions, please contact us <Anchor href='https://umich.qualtrics.com/jfe/form/SV_bCwYIKueEXs8wBf'>via our feedback form</Anchor>.</p>
       </main>
     </div>
   );
